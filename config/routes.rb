16e44ed3895897e955f46a31644f7723c8cab645
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-02-03 15:04:24 +0000

    PDF page UI with temp controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ AcceleratedClaims::Application.routes.draw do
   get '/new', controller: :claim, action: :new
   post '/submission', controller: :claim, action: :submission
   get '/thank_you', controller: :claim, action: :thank_you
+
+  # routes to temporary front-end layout
+  get '/pdf' => 'temp#pdf'
 end
