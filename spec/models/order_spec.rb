66baee8f27772a8358360c0bc818edf84aeb6197
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-04 17:27:44 +0000

    Order model added

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Order do
+  let(:order) { Order.new(possession: true, cost: true) }
+
+  describe "when given all valid values" do
+    it "should be valid" do
+      order.should be_valid
+    end
+  end
+
+  describe "possession" do
+    it "should not be blank" do
+      order.possession = ""
+      order.should_not be_valid
+    end
+  end
+
+  describe "cost" do
+    it "should not be blank" do
+      order.cost = ""
+      order.should_not be_valid
+    end
+  end
+
+end
