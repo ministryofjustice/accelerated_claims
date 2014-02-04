08d491960c95bd2d00b5ba3953efa116e9072f0e
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-04 15:25:31 +0000

    Tenant model added

---
+++ spec/models/tenant_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Tenant do
+  let(:tenant) { Tenant.new }
+
+  describe "when given all valid values" do
+    it "should be valid" do
+      Tenant.new(title: "Mr",
+                 full_name: "John Major",
+                 street: "Sesame Street",
+                 town: "London",
+                 postcode: "SW1").should be_valid
+    end
+  end
+
+  describe "title" do
+    it "when blank" do
+      tenant.title = ""
+      tenant.should_not be_valid
+    end
+
+    it "when over 8 characters long" do
+      tenant.title = "x" * 9
+      tenant.should_not be_valid
+    end
+  end
+
+
+end
