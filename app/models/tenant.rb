15b9eb3d06375c4cf19eb9be119dc11e7864efc8
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-04 15:25:31 +0000

    Tenant model added

---
+++ app/models/tenant.rb
@@ -0,0 +1,10 @@
+class Tenant
+  include ActiveModel::Model
+
+  attr_accessor :title
+  validates :title, presence: true, length: { maximum: 8 }
+  attr_accessor :full_name
+  attr_accessor :street
+  attr_accessor :town
+  attr_accessor :postcode
+end
