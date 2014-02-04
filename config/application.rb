f8a3355c2b593b6924d2ac4ebea28b37ab5c98c4
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-04 15:08:53 +0000

    Added Landlord form to the view
    Added 'active_model' explicitly in,
    otherwise the view would not render

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-
+require "active_model"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
