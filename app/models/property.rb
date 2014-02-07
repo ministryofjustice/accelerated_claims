fafd6ddc9fc4ebd8d24cdf6a2f568e8c4fd8d5f7
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-07 14:43:50 +0000

    Added tighter Postcode validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,5 @@
+require 'uk_postcode'
+
 class Property < BaseClass
 
   attr_accessor :street
@@ -8,8 +10,18 @@ class Property < BaseClass
 
   attr_accessor :postcode
   validates :postcode, presence: true, length: { maximum: 8 }
+  validate :full_postcode
+
+  def full_postcode
+    errors.add(:full_postcode, "not full postcode") unless UKPostcode.new(postcode).full?
+  end
 
   attr_accessor :house
   validates :house, presence: true
+
+  def as_json
+    pcode = UKPostcode.new(postcode)
+    { "property" => "#{street}, #{town}", "property_postcode1" => "#{pcode.outcode}", "property_postcode2" => "#{pcode.incode}" }
+  end
 
 end
