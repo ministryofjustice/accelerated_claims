663bd4cd7e122f7a63d60683971d82450bbdb86f
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-10 15:19:56 +0000

    Added Landlord data to PDF form filling
    Added Landlord model to ClaimData so that it formats
    it's values for PDF filling

--- app/lib/claim_data.rb
+++ app/lib/claim_data.rb
@@ -2,11 +2,13 @@ class ClaimData
 
   def initialize(params)
     @property = Property.new(params["claim"]["property"])
+    @landlord = Landlord.new(params["claim"]["landlord"])
   end
 
   def formatted
     data = {}
     data.merge! @property.as_json
+    data.merge! @landlord.as_json
     data
   end
 end
