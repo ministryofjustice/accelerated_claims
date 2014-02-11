9c8761ab6938b2fada6fe8f63fa193c2a71a8694
Tom Gladhill <whoojemaflip@gmail.com>
2014-02-11 17:26:25 +0000

    handling dymanically created attributes of claim model

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -7,34 +7,34 @@ class Claim < BaseClass
 
   def as_json()
     json = {}
-    self.instance_variables.each { |var| json.merge! instance_variable_get(var).as_json }
+    attributes_from_submodels.each { |var, model| json.merge! instance_variable_get("@#{var}").as_json }
     json
   end
 
   private
+
+  def submodels
+    %w(Property Landlord DemotedTenancy Notice License Deposit Defendant Order Tenant)
+  end
+
+  def attributes_from_submodels
+    attributes = {} 
+    submodels.reject { |m| m == 'Tenant' }.each { |model| attributes[model.underscore] = model }
+    %w(one two).each { |n| attributes["tenant_#{n}"] = 'Tenant' }
+    attributes
+  end
+
+  def initialize_all_submodels(params)
+    attributes_from_submodels.each { |attribute, model|
+      init_submodel(params, attribute, model)
+    }
+  end
 
   def init_submodel(claim_params, instance_var, model)
     sub_params = claim_params.has_key?(instance_var) ? claim_params[instance_var] : {}
     instance_variable_set("@#{instance_var}", model.constantize.new(sub_params))
     self.class.send( :define_method, instance_var.to_sym) {
       instance_variable_get "@#{instance_var}"
-    }
-  end
-
-  def submodels
-    %w(Property Landlord DemotedTenancy Notice License Deposit Defendant Order)
-  end
-
-  def tenant_submodels
-    %w(one two)
-  end
-
-  def initialize_all_submodels(params)
-    submodels.each { |model|
-      init_submodel(params, model.underscore, model)
-    }
-    tenant_submodels.each { |num|
-      init_submodel(params, "tenant_#{num}", 'Tenant')
     }
   end
 end
