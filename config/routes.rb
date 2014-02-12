2c14fd1307f420d6de5edfa06ce3c440907a6800
Tom Gladhill <whoojemaflip@gmail.com>
2014-02-12 18:16:02 +0000

    Scoped routes to RAILS_RELATIVE_URL_ROOT

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 AcceleratedClaims::Application.routes.draw do
-  root 'claim#new'
-  get '/new', controller: :claim, action: :new
-  post '/submission', controller: :claim, action: :submission
-
-  # routes to temporary front-end layout
-  get '/pdf' => 'temp#pdf'
-  get '/form' => 'temp#form'
+  scope ENV["RAILS_RELATIVE_URL_ROOT"] || '/' do
+    root 'claim#new'
+    get '/new', controller: :claim, action: :new
+    post '/submission', controller: :claim, action: :submission
+  end
 end
