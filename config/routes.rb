a8da863024399bdedb364531fdfe08e3103450a1
Tom Gladhill <whoojemaflip@gmail.com>
2014-02-12 18:32:53 +0000

    More base path madness

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 AcceleratedClaims::Application.routes.draw do
-  scope ENV["RAILS_RELATIVE_URL_ROOT"] || '/' do
+  scope AcceleratedClaims::Application.config.relative_url_root || '/' do
     root 'claim#new'
     get '/new', controller: :claim, action: :new
     post '/submission', controller: :claim, action: :submission
