d80e83a6e3995421478a103ebd6ea0e42821809a
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-13 17:02:04 +0000

    Tenancy data for spec support added

--- spec/models/tenancy_spec.rb
+++ spec/models/tenancy_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe Tenancy do
   let(:tenancy) do
     Tenancy.new(start_date: "01 01 2010",
-               latest_agreement_date: "01 01 2010",
-               agreement_reissued_for_same_property: false,
-               agreement_reissued_for_same_landlord_and_tenant: false)
+                latest_agreement_date: "01 01 2010",
+                agreement_reissued_for_same_property: false,
+                agreement_reissued_for_same_landlord_and_tenant: false)
   end
 
   describe "when given all valid values" do
