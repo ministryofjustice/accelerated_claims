6f94e13f638d4ca5621ee5402b7f3a8885197836
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-13 17:31:59 +0000

    Tenancy model's as_json
    So that the data can feed into the PDF form

--- app/models/tenancy.rb
+++ app/models/tenancy.rb
@@ -10,4 +10,21 @@ class Tenancy < BaseClass
 
   attr_accessor :agreement_reissued_for_same_landlord_and_tenant
   validates :agreement_reissued_for_same_landlord_and_tenant, inclusion: { in: [true, false] }
+
+  def as_json
+    day = '%d'
+    month = '%m'
+    year = '%Y'
+
+    {
+      "start_date_day" => "#{start_date.strftime(day)}",
+      "start_date_month" => "#{start_date.strftime(month)}",
+      "start_date_year" => "#{start_date.strftime(year)}",
+      "latest_agreement_date_day" => "#{latest_agreement_date.strftime(day)}",
+      "latest_agreement_date_month" => "#{latest_agreement_date.strftime(month)}",
+      "latest_agreement_date_year" => "#{latest_agreement_date.strftime(year)}",
+      "agreement_reissued_for_same_property" => agreement_reissued_for_same_property,
+      "agreement_reissued_for_same_landlord_and_tenant" => agreement_reissued_for_same_landlord_and_tenant
+    }
+  end
 end
