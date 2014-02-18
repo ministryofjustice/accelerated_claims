105e3b4b5de898a2292320a9932ef83460f996b4
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-02-18 09:56:54 +0000

    Added Capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] ||= 'test'
 ENV["PDFTK"] ||= '/usr/local/bin/pdftk'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
