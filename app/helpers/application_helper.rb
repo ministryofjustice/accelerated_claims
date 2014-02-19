12358f4ca1f83fa2896a0c8730d408f89da04649
Rob McKinnon <@digital.justice.gov.uk>
2014-02-19 17:16:45 +0000

    helper to render row with error; made partials for claimant and defendant

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def row css_selector, field, model
+    div = "div.row#{css_selector}"
+    div += '.error' if model.errors.messages.key?(field)
+
+    haml_tag div do
+      yield
+    end
+  end
+
 end
