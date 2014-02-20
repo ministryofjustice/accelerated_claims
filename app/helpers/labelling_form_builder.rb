8e28656ce33a3f3e3577f1f007e8f1d79e1a5393
Rob McKinnon <@digital.justice.gov.uk>
2014-02-20 17:32:49 +0000

    use custom form builder for claimant

--- app/helpers/labelling_form_builder.rb
+++ app/helpers/labelling_form_builder.rb
@@ -1,10 +1,10 @@
 class LabellingFormBuilder < ActionView::Helpers::FormBuilder
 
   def text_field_row(attribute, options={})
-    @template.row_input options[:class], attribute, @object, self, :text_field
+    @template.row_input options[:class], options[:input_class], attribute, @object, self, :text_field, options[:label]
   end
 
   def text_area_row(attribute, options={})
-    @template.row_input options[:class], attribute, @object, self, :text_area
+    @template.row_input options[:class], options[:input_class], attribute, @object, self, :text_area, options[:label]
   end
 end
--- app/models/claimant.rb
+++ app/models/claimant.rb
@@ -4,10 +4,12 @@ class Claimant < BaseClass
   validates :company, presence: true
 
   attr_accessor :street
+  validates :street, presence: true
   validates :street, length: { maximum: 40 }
 
   attr_accessor :town
   attr_accessor :postcode
+  validates :postcode, presence: true
 
   def as_json
     postcode1, postcode2 = split_postcode
