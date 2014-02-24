75ad43498dbb8fd9ddaba139f70062d43a025184
Rob McKinnon <@digital.justice.gov.uk>
2014-02-24 15:44:36 +0000

    use radio_button_fieldset() helper for yes/no questions

--- app/models/possession.rb
+++ app/models/possession.rb
@@ -1,4 +1,4 @@
 class Possession < BaseClass
   attr_accessor :hearing
-  validates :hearing, inclusion: { in: ['Yes', 'No'] }
+  validates :hearing, presence: true, inclusion: { in: ['Yes', 'No'] }
 end
--- app/models/tenancy.rb
+++ app/models/tenancy.rb
@@ -6,10 +6,10 @@ class Tenancy < BaseClass
   attr_accessor :latest_agreement_date
 
   attr_accessor :agreement_reissued_for_same_property
-  validates :agreement_reissued_for_same_property, inclusion: { in: ['Yes', 'No'] }
+  validates :agreement_reissued_for_same_property, presence: true, inclusion: { in: ['Yes', 'No'] }
 
   attr_accessor :agreement_reissued_for_same_landlord_and_tenant
-  validates :agreement_reissued_for_same_landlord_and_tenant, inclusion: { in: ['Yes', 'No'] }
+  validates :agreement_reissued_for_same_landlord_and_tenant, presence: true, inclusion: { in: ['Yes', 'No'] }
 
   def as_json
     day = '%d'
