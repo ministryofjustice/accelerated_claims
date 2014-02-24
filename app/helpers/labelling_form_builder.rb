d35aedfdca38f2a7964d37f0026271ad96b9f6c0
Rob McKinnon <@digital.justice.gov.uk>
2014-02-24 10:56:11 +0000

    rename field to attribute

--- app/helpers/labelling_form_builder.rb
+++ app/helpers/labelling_form_builder.rb
@@ -8,18 +8,18 @@ class LabellingFormBuilder < ActionView::Helpers::FormBuilder
     row_input options[:class], options[:input_class], attribute, @object, self, :text_area, options[:label]
   end
 
-  def row css_selector, field, model
+  def row css_selector, attribute, model
     div = "div.row#{css_selector}"
-    div += '.error' if model.errors.messages.key?(field)
+    div += '.error' if model.errors.messages.key?(attribute)
 
     @template.haml_tag div do
-      yield field, presence_required?(field, model)
+      yield attribute, presence_required?(attribute, model)
     end
   end
 
-  def fieldset css_selector, field, model
+  def fieldset css_selector, attribute, model
     fieldset = "fieldset#{css_selector}"
-    fieldset += '.error' if model.errors.messages.key?(field)
+    fieldset += '.error' if model.errors.messages.key?(attribute)
 
     @template.haml_tag fieldset do
       yield
@@ -28,28 +28,28 @@ class LabellingFormBuilder < ActionView::Helpers::FormBuilder
 
   private
 
-  def row_input css_selector, input_class, field, model, form, input, label=nil
+  def row_input css_selector, input_class, attribute, model, form, input, label=nil
     css = "row #{css_selector.gsub('.',' ')}"
-    css += ' error' if model.errors.messages.key?(field)
+    css += ' error' if model.errors.messages.key?(attribute)
 
     @template.surround("<div class='#{css}'>".html_safe,"</div>".html_safe) do
-      labelled_input field, input_class, form, input, presence_required?(field, model), label
+      labelled_input attribute, input_class, form, input, presence_required?(attribute, model), label
     end
   end
 
-  def labelled_input field, input_class, form, input, required, label=nil
-    label ||= field
-    [ form.label(field, label_for(label, required)), form.send(input,field, class: input_class) ].join("\n").html_safe
+  def labelled_input attribute, input_class, form, input, required, label=nil
+    label ||= attribute
+    [ form.label(attribute, label_for(label, required)), form.send(input, attribute, class: input_class) ].join("\n").html_safe
   end
 
-  def label_for field, required
-    label = field.to_s.humanize
+  def label_for attribute, required
+    label = attribute.to_s.humanize
     label = %Q|#{label}<span class="req">*</span>| if required
     label.html_safe
   end
 
-  def presence_required? field, model
-    model.class.validators_on(field).any? {|v| v.is_a?(ActiveModel::Validations::PresenceValidator)}
+  def presence_required? attribute, model
+    model.class.validators_on(attribute).any? {|v| v.is_a?(ActiveModel::Validations::PresenceValidator)}
   end
 
 end
