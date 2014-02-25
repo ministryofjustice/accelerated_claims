6d6723f9aef9ee3b7c7ffb9a2c235c1e88d600b7
Rob McKinnon <@digital.justice.gov.uk>
2014-02-25 13:54:49 +0000

    ignore invalid attributes in form post

--- app/models/base_class.rb
+++ app/models/base_class.rb
@@ -32,8 +32,9 @@ class BaseClass
           dattrs[an] = [] if dattrs[an].nil?
           dattrs[an] << { :n => n, :v => v }
         else
-          send( "#{n}=", v)          
-        end          
+          writer = :"#{n}="
+          send( writer, v) if respond_to?(writer)
+        end
       end
       dattrs.each do |k, v|
         vs = v.sort_by{|hv| hv[:n] }.collect{|hv| hv[:v] }
@@ -43,6 +44,6 @@ class BaseClass
         dv = [ p1, p2, p3 ].join( "-" )
         begin send( "#{k}=", Date.parse( dv ) ); rescue; end
       end
-    end       
+    end
   end
 end
