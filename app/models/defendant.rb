65b604280343fe16c0c4238945b48072c9ccccc8
Rob McKinnon <@digital.justice.gov.uk>
2014-03-04 12:16:35 +0000

    use Address concern to validate address fields

--- app/models/defendant.rb
+++ app/models/defendant.rb
@@ -1,13 +1,11 @@
 class Defendant < BaseClass
 
+  include Address
+
   attr_accessor :validate_presence
 
   attr_accessor :title
   attr_accessor :full_name
-
-  attr_accessor :street
-  attr_accessor :town
-  attr_accessor :postcode
 
   with_options if: :validate_presence do |defendant|
     defendant.validates :title, presence: { message: 'must be entered' }
@@ -16,8 +14,6 @@ class Defendant < BaseClass
 
   validates :title, length: { maximum: 8 }
   validates :full_name, length: { maximum: 40 }
-  validates :street, length: { maximum: 40 }
-  validates :postcode, length: { maximum: 8 }
 
   def as_json
     postcode1, postcode2 = split_postcode
