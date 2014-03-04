f56a3ed9d98a684f070fe34984b049c4934ffd4a
Rob McKinnon <@digital.justice.gov.uk>
2014-03-04 15:55:43 +0000

    fields for assured shorthold tenancy notice

--- app/models/tenancy.rb
+++ app/models/tenancy.rb
@@ -6,6 +6,8 @@ class Tenancy < BaseClass
   attr_accessor :latest_agreement_date
   attr_accessor :reissued_for_same_property
   attr_accessor :reissued_for_same_landlord_and_tenant
+  attr_accessor :assured_shorthold_tenancy_notice_served_by
+  attr_accessor :assured_shorthold_tenancy_notice_served_date
 
   validates :start_date, presence: { message: 'must be entered' }, unless: :demoted_tenancy
 
@@ -27,7 +29,11 @@ class Tenancy < BaseClass
       "latest_agreement_date_month" => month(latest_agreement_date),
       "latest_agreement_date_year" => year(latest_agreement_date),
       "agreement_reissued_for_same_property" => reissued_for_same_property,
-      "agreement_reissued_for_same_landlord_and_tenant" => reissued_for_same_landlord_and_tenant
+      "agreement_reissued_for_same_landlord_and_tenant" => reissued_for_same_landlord_and_tenant,
+      'assured_shorthold_tenancy_notice_served_by' => assured_shorthold_tenancy_notice_served_by,
+      'assured_shorthold_tenancy_notice_served_date_day' => day(assured_shorthold_tenancy_notice_served_date),
+      'assured_shorthold_tenancy_notice_served_date_month' => month(assured_shorthold_tenancy_notice_served_date),
+      'assured_shorthold_tenancy_notice_served_date_year' => year(assured_shorthold_tenancy_notice_served_date)
     }
   end
 end
