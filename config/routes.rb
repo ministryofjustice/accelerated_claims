b55d672c27db7188e67102d475b62a0766503dd4
Tom Gladhill <whoojemaflip@gmail.com>
2014-03-04 16:31:52 +0000

    Added tests for landing page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 AcceleratedClaims::Application.routes.draw do
   
   scope AcceleratedClaims::Application.config.relative_url_root || '/' do
-    root 'claim#new'
-    get '/new',           controller: :claim, action: :new
-    post '/submission',   controller: :claim, action: :submission
-    get '/confirmation',  controller: :claim, action: :confirmation
-    get '/download',      controller: :claim, action: :download
-    get '/landing',       controller: :claim, action: :landing
+    root 'claim#landing'
 
+    get  '/',              controller: :claim, action: :landing
+    get  '/new',           controller: :claim, action: :new
+    post '/submission',    controller: :claim, action: :submission
+    get  '/confirmation',  controller: :claim, action: :confirmation
+    get  '/download',      controller: :claim, action: :download
 
+    # zendesk
     resource :feedback,   only: [:new, :create], controller: 'feedback' 
   end
 end
