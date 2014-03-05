8f1e1531f82096b74e2e23c270a107d4ddb59aff
Rob McKinnon <@digital.justice.gov.uk>
2014-03-05 17:21:53 +0000

    show deliver up possession validation error message in form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < BaseClass
 
   attr_accessor :possession
-  validates :possession, presence: true, inclusion: { in: ['Yes'] }
+  validates :possession, presence: true
+  validates :possession, inclusion: { in: ['Yes'], message: 'must be checked' }
 
   attr_accessor :cost
   validates :cost, presence: true
