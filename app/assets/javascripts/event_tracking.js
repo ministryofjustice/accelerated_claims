757338b2a1313401cfe04d06a4331adb8205f995
Tom Gladhill <whoojemaflip@gmail.com>
2014-03-05 13:35:15 +0000

    custom event tracking

---
+++ app/assets/javascripts/event_tracking.js
@@ -0,0 +1,59 @@
+/*jslint browser: true, evil: false, plusplus: true, white: true, indent: 2 */
+/*global moj, $, Handlebars */
+
+moj.Modules.eventTracking = (function() {
+  "use strict";
+
+  var //functions
+      init,
+      cacheEls,
+      bindEvents,
+      eventCategory,
+      dispatchTrackingEvent,
+
+      // vars
+      tracked_elements
+      ;
+
+  init = function() {
+    cacheEls();
+    bindEvents();
+  };
+
+  cacheEls = function() {
+    tracked_elements = $( '[data-event-name]' );
+  };
+
+  bindEvents = function() {
+    tracked_elements.on( 'click', function() {
+      var category = eventCategory( this ),
+          label    = $( this ).data( 'event-label' );
+
+      dispatchTrackingEvent( category, 'click', label );
+    });
+  };
+
+  dispatchTrackingEvent = function( category, event, label ) {
+    if( typeof ga == 'function' ) { // google analytics
+      ga( 'send', 'event', category, event, label );
+    }
+    if( typeof _paq == 'function' ) { // piwik
+      _paq.push( ['trackPageView', category+'/'+label] )
+    }
+  }
+
+  eventCategory = function( el ) {
+    var cat = 'undefined';
+    if( $( el ).hasClass( 'button' ) || $( el ).get(0).tagName == 'button' ) {
+      cat = 'button';
+    }
+    return cat;
+  }
+
+  // public
+
+  return {
+    init: init
+  };
+
+}());
