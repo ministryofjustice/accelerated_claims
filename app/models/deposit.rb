4e522a2ba45e4bfd33a0bbce0d964c93831fa310
Rob McKinnon <@digital.justice.gov.uk>
2014-03-11 15:21:23 +0000

    added information_given_date to deposit model

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -3,8 +3,21 @@ class Deposit < BaseClass
   attr_accessor :received
   validates :received, presence: { message: 'must be selected' }, inclusion: { in: ['Yes', 'No'] }
 
+  attr_accessor :information_given_date
+
   attr_accessor :ref_number
 
   attr_accessor :as_property
   validates :as_property, presence: { message: 'must be selected' }, inclusion: { in: ['Yes', 'No'] }
+
+  def as_json
+    json = super
+    json.delete('information_given_date')
+    json.merge({
+      "information_given_date_day" => day(information_given_date),
+      "information_given_date_month" => month(information_given_date),
+      "information_given_date_year" => year(information_given_date)
+    }
+    )
+  end
 end
--- spec/models/deposit_spec.rb
+++ spec/models/deposit_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Deposit do
-  let(:deposit) { Deposit.new(received: 'Yes', ref_number: 123, as_property: 'No') }
+  let(:deposit) { Deposit.new(received: 'Yes', ref_number: 'x123', as_property: 'No', information_given_date: Date.parse("2010-01-10")) }
 
   describe "when given all valid values" do
     it "should be valid" do
@@ -20,6 +20,19 @@ describe Deposit do
     it "shouldn't be valid" do
       deposit.as_property = ""
       deposit.should_not be_valid
+    end
+  end
+
+  describe 'as_json' do
+    it 'should return correct json' do
+      deposit.as_json.should == {
+        "as_property" => "No",
+        "information_given_date_day"=>"10",
+        "information_given_date_month"=>"01",
+        "information_given_date_year"=>"2010",
+        "received" => "Yes",
+        "ref_number" => 'x123'
+      }
     end
   end
 end
