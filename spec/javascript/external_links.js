6c691c96d8b3a4029353e92235fb4e5977aa6806
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-03-11 11:07:32 +0000

    Fix JS test for external links

--- spec/javascript/external_links.js
+++ spec/javascript/external_links.js
@@ -9,12 +9,19 @@ casper.waitForPopup('https://www.possessionclaim.gov.uk/pcol/', function() {
     this.test.assertEquals(this.popups.length, 1);
 });
 
+
 casper.thenOpen(url + '/new', function() {
-  this.test.comment('Testing external link on form page');
-  this.click('a.external');
+  this.test.comment('Testing external links on form page');
+  this.click('a#giving-notice-link');
+});
+casper.waitForPopup('https://www.gov.uk/gaining-possession-of-a-privately-rented-property-let-on-an-assured-shorthold-tenancy#seeking-possession-under-section-21', function() {
+    this.test.assertEquals(this.popups.length, 2);
+});
+casper.then(function() {
+  this.click('a#multiple-occupation-link');
 });
 casper.waitForPopup('https://www.gov.uk/private-renting/houses-in-multiple-occupation', function() {
-    this.test.assertEquals(this.popups.length, 2);
+    this.test.assertEquals(this.popups.length, 3);
 });
 
 casper.thenOpen(url + '/confirmation', function() {
@@ -22,7 +29,7 @@ casper.thenOpen(url + '/confirmation', function() {
   this.click('a.external');
 });
 casper.waitForPopup('https://courttribunalfinder.service.gov.uk/', function() {
-    this.test.assertEquals(this.popups.length, 3);
+    this.test.assertEquals(this.popups.length, 4);
 });
 
 
