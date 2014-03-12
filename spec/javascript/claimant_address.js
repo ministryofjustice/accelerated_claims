f83d161bb5fd2e9cf2ac3d924e90869726379a6d
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-03-12 14:57:57 +0000

    Multiple person selectors for claimant and defendant

--- spec/javascript/claimant_address.js
+++ spec/javascript/claimant_address.js
@@ -5,6 +5,9 @@ casper.start(url + '/new', function() {
   casper.test.comment('Testing claimant 2 address functionality');
 
   this.test.assertNotVisible('.same-address .address');
+  this.evaluate(function(){
+    $('#multiplePanelSelector_claimants').val('2').trigger('change');
+  });
   this.click('#claimant2address-no');
   this.test.assertVisible('.same-address .address');
 
