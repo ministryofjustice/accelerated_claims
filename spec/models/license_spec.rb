cbd6f2b1de1eb97d1df50526c04d1aaff327ce81
Rob McKinnon <@digital.justice.gov.uk>
2014-03-12 17:33:32 +0000

    part way through license section changes

--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -1,94 +1,103 @@
 require 'spec_helper'
 
 describe License do
+
   let(:license) do
-    License.new(multiple_occupation: 'Yes',
-                multiple_occupation_authority: "Westminster City",
-                multiple_occupation_date: Date.parse("2013-01-01"),
-                housing_act: 'Yes',
-                housing_act_authority: "Westminster City",
-                housing_act_date: Date.parse("2013-01-01")
-              )
+    License.new({
+        multiple_occupation: multiple_occupation,
+        license_issued_under: license_issued_under,
+        license_issued_by: license_issued_by,
+      }.merge(
+        form_date 'license_issued_date', license_issued_date
+      )
+    )
   end
 
-  let(:desired_format) do
-    {
-      "hmo" => 'Yes',
-      "authority" => "Westminster City",
-      "hmo_day" => "01",
-      "hmo_month" => "01",
-      "hmo_year" => "2013",
-      "housing_act" => 'Yes',
-      "housing_act_authority" => "Westminster City",
-      "housing_act_date_day" => "01",
-      "housing_act_date_month" => "01",
-      "housing_act_date_year" => "2013"
+  subject { license }
+
+  context 'in multiple occupation' do
+    let(:multiple_occupation) { 'Yes' }
+    let(:license_issued_by) { 'Westminster City' }
+    let(:license_issued_date) { Date.parse("2013-01-12") }
+    let(:license_issued_under) { 'Part2' }
+
+    context 'and licensed under part 2 of act' do
+      it { should be_valid }
+      its(:as_json) { should == {
+          "hmo" => 'Yes',
+          "authority" => license_issued_by,
+          "hmo_day" => '12',
+          "hmo_month" => '01',
+          "hmo_year" => '2013',
+          "housing_act" => 'No',
+          "housing_act_authority" => '',
+          "housing_act_date_day" => '',
+          "housing_act_date_month" => '',
+          "housing_act_date_year" => ''
+        }
+      }
+    end
+
+    context 'and licensed under part 3 of act' do
+      let(:license_issued_under) { 'Part3' }
+      it { should be_valid }
+      its(:as_json) { should == {
+          "hmo" => 'No',
+          "authority" => '',
+          "hmo_day" => '',
+          "hmo_month" => '',
+          "hmo_year" => '',
+          "housing_act" => 'Yes',
+          "housing_act_authority" => license_issued_by,
+          "housing_act_date_day" => '12',
+          "housing_act_date_month" => '01',
+          "housing_act_date_year" => '2013'
+        }
+      }
+    end
+
+    context 'and license_issued_under invalid' do
+      let(:license_issued_under) { 'Part100' }
+      it { should_not be_valid }
+    end
+
+    context 'and license_issued_under blank' do
+      let(:license_issued_under) { '' }
+      it { should_not be_valid }
+    end
+
+    context 'and license_issued_by blank' do
+      let(:license_issued_by) { '' }
+      it { should_not be_valid }
+    end
+
+    context 'and license_issued_date blank' do
+      let(:license_issued_date) { '' }
+      it { should_not be_valid }
+    end
+  end
+
+  context 'not in multiple occupation' do
+    let(:multiple_occupation) { 'No' }
+    let(:license_issued_date) { '' }
+    let(:license_issued_by) { '' }
+    let(:license_issued_under) { '' }
+
+    it { should be_valid }
+
+    its(:as_json) { should == {
+        "hmo" => 'No',
+        "authority" => '',
+        "hmo_day" => '',
+        "hmo_month" => '',
+        "hmo_year" => '',
+        "housing_act" => 'No',
+        "housing_act_authority" => '',
+        "housing_act_date_day" => '',
+        "housing_act_date_month" => '',
+        "housing_act_date_year" => ''
+      }
     }
   end
 
-  describe "#as_json" do
-    it "should produce formatted output" do
-      expect(license.as_json).to eq desired_format
-    end
-  end
-
-  describe "when given all valid values" do
-    it "should be valid" do
-      license.should be_valid
-    end
-  end
-
-  describe "house in multiple occupation value" do
-    it "can't be blank" do
-      license.multiple_occupation = ""
-      license.should_not be_valid
-    end
-
-    describe "when HMO is present" do
-      before { license.multiple_occupation_authority = "" }
-
-      it "should require authority name" do
-        license.should_not be_valid
-      end
-    end
-  end
-
-  describe "the value for Housing Act 2004" do
-    it "can't be blank" do
-      license.housing_act = ""
-      license.should_not be_valid
-    end
-
-    describe "when property is licensed under Part 3 of Housing Act 2004" do
-      before { license.housing_act_authority = "" }
-
-      it "should require housing act authority" do
-        license.should_not be_valid
-      end
-    end
-  end
-
-  describe "the housing act date value" do
-    it "can be blank" do
-      license.housing_act_date = nil
-      json_mod = { 
-        'housing_act_date_day' => '', 
-        'housing_act_date_month' => '', 
-        'housing_act_date_year' => '' 
-      }
-      expect(license.as_json).to eql desired_format.merge(json_mod)
-    end
-  end
-
-  describe "the multiple_occupation_date value" do
-    it "can be blank" do
-      license.multiple_occupation_date = nil
-      json_mod = {
-        'hmo_day' => '', 
-        'hmo_month' => '', 
-        'hmo_year' => ''
-      }
-      expect(license.as_json).to eql desired_format.merge(json_mod)
-    end
-  end
 end
