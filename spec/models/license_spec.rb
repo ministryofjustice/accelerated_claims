e91ce761be21747e769150f13dd21b40e841ef50
Rob McKinnon <@digital.justice.gov.uk>
2014-03-13 13:14:47 +0000

    shorted license attribute names

--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -5,10 +5,10 @@ describe License do
   let(:license) do
     License.new({
         multiple_occupation: multiple_occupation,
-        license_issued_under: license_issued_under,
-        license_issued_by: license_issued_by,
+        issued_under_act_part: issued_under_act_part,
+        issued_by: issued_by,
       }.merge(
-        form_date 'license_issued_date', license_issued_date
+        form_date 'issued_date', issued_date
       )
     )
   end
@@ -17,15 +17,15 @@ describe License do
 
   context 'in multiple occupation' do
     let(:multiple_occupation) { 'Yes' }
-    let(:license_issued_by) { 'Westminster City' }
-    let(:license_issued_date) { Date.parse("2013-01-12") }
-    let(:license_issued_under) { 'Part2' }
+    let(:issued_by) { 'Westminster City' }
+    let(:issued_date) { Date.parse("2013-01-12") }
+    let(:issued_under_act_part) { 'Part2' }
 
     context 'and licensed under part 2 of act' do
       it { should be_valid }
       its(:as_json) { should == {
           "hmo" => 'Yes',
-          "authority" => license_issued_by,
+          "authority" => issued_by,
           "hmo_day" => '12',
           "hmo_month" => '01',
           "hmo_year" => '2013',
@@ -39,7 +39,7 @@ describe License do
     end
 
     context 'and licensed under part 3 of act' do
-      let(:license_issued_under) { 'Part3' }
+      let(:issued_under_act_part) { 'Part3' }
       it { should be_valid }
       its(:as_json) { should == {
           "hmo" => 'No',
@@ -48,7 +48,7 @@ describe License do
           "hmo_month" => '',
           "hmo_year" => '',
           "housing_act" => 'Yes',
-          "housing_act_authority" => license_issued_by,
+          "housing_act_authority" => issued_by,
           "housing_act_date_day" => '12',
           "housing_act_date_month" => '01',
           "housing_act_date_year" => '2013'
@@ -56,32 +56,32 @@ describe License do
       }
     end
 
-    context 'and license_issued_under invalid' do
-      let(:license_issued_under) { 'Part100' }
+    context 'and issued_under_act_part invalid' do
+      let(:issued_under_act_part) { 'Part100' }
       it { should_not be_valid }
     end
 
-    context 'and license_issued_under blank' do
-      let(:license_issued_under) { '' }
+    context 'and issued_under_act_part blank' do
+      let(:issued_under_act_part) { '' }
       it { should_not be_valid }
     end
 
-    context 'and license_issued_by blank' do
-      let(:license_issued_by) { '' }
+    context 'and issued_by blank' do
+      let(:issued_by) { '' }
       it { should_not be_valid }
     end
 
-    context 'and license_issued_date blank' do
-      let(:license_issued_date) { '' }
+    context 'and issued_date blank' do
+      let(:issued_date) { '' }
       it { should_not be_valid }
     end
   end
 
   context 'not in multiple occupation' do
     let(:multiple_occupation) { 'No' }
-    let(:license_issued_date) { '' }
-    let(:license_issued_by) { '' }
-    let(:license_issued_under) { '' }
+    let(:issued_date) { '' }
+    let(:issued_by) { '' }
+    let(:issued_under_act_part) { '' }
 
     it { should be_valid }
 
