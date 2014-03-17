c270c1643c77528de87d13344b68eb97adf439c0
Rob McKinnon <@digital.justice.gov.uk>
2014-03-17 14:59:54 +0000

    move casper install check to ci2.sh

--- scripts/ci2.sh
+++ scripts/ci2.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if hash npm 2>/dev/null; then
+  version=`npm -v`
+  echo "node installed: $version"
+
+  version=`casperjs --version`
+  echo "casperjs installed: $version"
+
+  if [ -z $version ]; then
+    echo >&2 "requires casperjs but it's not installed. Aborting. Try: npm install -g casperjs"; exit 1;
+  fi
+else
+  echo 'node not installed'
+fi
+
 echo 'running rails server'
 bundle exec rails s -p 5555 &
 rails_pid=$!
