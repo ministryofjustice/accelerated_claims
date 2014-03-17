e2f95ccc88919cb735a7ef0f82bfde10c8e91443
Rob McKinnon <@digital.justice.gov.uk>
2014-03-17 14:20:38 +0000

    script for casperjs testing

---
+++ scripts/ci2.sh
@@ -0,0 +1,16 @@
+# This runs CasperJS tests, but doesn't work at present.
+
+function at_exit() {
+  echo `ps -Af | grep rails | grep -v grep`
+  echo 'killing process...'
+  kill `cat application.pid`
+  sleep 1
+  echo `ps -Af | grep rails | grep -v grep`
+}
+trap at_exit 0
+
+daemonize -c . -l application.lock -v -p application.pid `which bundle` exec rails s -p 5555
+sleep 10
+
+casperjs test ./spec/javascript/ --url='http://localhost:5555/' --no-colors --xunit=log.xml
+
