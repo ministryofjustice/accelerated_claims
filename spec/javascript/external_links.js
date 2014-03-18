bb26e05941b6de0b15e06e097924dbd8f7f310dc
Rob McKinnon <@digital.justice.gov.uk>
2014-03-18 10:40:01 +0000

    moved form external link tests into separate file to fix test failure

--- spec/javascript/external_links.js
+++ spec/javascript/external_links.js
@@ -1,6 +1,5 @@
 var url = casper.cli.get('url');
 
-
 casper.start(url, function() {
   this.test.comment('Testing external links on landing page');
   this.click('a#find-out-more');
@@ -31,25 +30,6 @@ casper.then(function(){
 });
 casper.waitForPopup('http://www.adviceguide.org.uk', function() {
   this.test.assertEquals(this.popups.length, 5);
-});
-
-
-
-
-casper.thenOpen(url + '/new', function() {
-  this.test.comment('Testing external links on form page');
-  this.click('a#giving-notice-link');
-});
-casper.waitForPopup('https://www.gov.uk/gaining-possession-of-a-privately-rented-property-let-on-an-assured-shorthold-tenancy#seeking-possession-under-section-21', function() {
-  this.test.assertEquals(this.popups.length, 6);
-});
-
-casper.thenOpen(url + '/confirmation', function() {
-  this.test.comment('Testing external link on confirmation page');
-  this.click('a.external');
-});
-casper.waitForPopup('https://courttribunalfinder.service.gov.uk/', function() {
-  this.test.assertEquals(this.popups.length, 7);
 });
 
 
