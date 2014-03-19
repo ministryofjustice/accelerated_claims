e1dcd099fea9e5b252282b1b2c7b94c90654030e
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-03-19 15:24:47 +0000

    idiot

--- scripts/ci2.sh
+++ scripts/ci2.sh
@@ -23,7 +23,7 @@ rails_pid=$!
 # Make sure the rails server is stopped when this script finishes
 trap "kill $rails_pid" EXIT
 
-sleep 10
-echo 'running casper tests'
-casperjs test ./spec/javascript/ --url='http://localhost:5555/' --no-colors --xunit=log.xml
+# sleep 10
+# echo 'running casper tests'
+# casperjs test ./spec/javascript/ --url='http://localhost:5555/' --no-colors --xunit=log.xml
 
