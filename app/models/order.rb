cc173ac6538ae143bd121a722798aa5c67470735
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-03-19 15:16:52 +0000

    Reverting back all the 'no's to 'No's
    There was only one exception where the *No* value was turned into a
    lowercase 'no'. The fix would've been to revert only that change.  But
    since all the 'No's (uppercase) were turned into 'no's (lowercase), this
    change reverts them back to the correct state. As all the 'Yes' options
    are already capitalised in the views as well as in the PDF. The same
    applies to the 'No' (options).

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,5 +6,5 @@ class Order < BaseClass
 
   attr_accessor :cost
   validates :cost, presence: true
-  validates :cost, inclusion: { in: ['Yes', 'no'] }
+  validates :cost, inclusion: { in: ['Yes', 'No'] }
 end
