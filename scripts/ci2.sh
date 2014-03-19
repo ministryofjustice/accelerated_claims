c1728628acf60e718fa77c4c8b2708d9e96405c9
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-03-19 15:33:56 +0000

    Don't need rails server

--- scripts/ci2.sh
+++ scripts/ci2.sh
@@ -16,12 +16,12 @@
 #   echo 'node not installed'
 # fi
 
-echo 'running rails server'
-bundle exec rails s -p 5555 &
-rails_pid=$!
+# echo 'running rails server'
+# bundle exec rails s -p 5555 &
+# rails_pid=$!
 
-# Make sure the rails server is stopped when this script finishes
-trap "kill $rails_pid" EXIT
+# # Make sure the rails server is stopped when this script finishes
+# trap "kill $rails_pid" EXIT
 
 # sleep 10
 # echo 'running casper tests'
