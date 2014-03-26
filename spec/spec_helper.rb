63da899c76aa8309da61abd6e684dbe57a3af7ad
Tom Gladhill <whoojemaflip@gmail.com>
2014-03-26 15:13:24 +0000

    poltergeist for remote feature tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,16 +6,38 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+<<<<<<< HEAD
+=======
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app,
+    :phantomjs_options => ['--ignore-ssl-errors=yes'],
+    :phantomjs_logger => open('/dev/null')
+  )
+end
+
+Capybara.javascript_driver = :poltergeist
+
+if remote = ENV.has_key?('remote_host')
+  Capybara.app_host = ENV['remote_host']
+  Capybara.default_driver = Capybara.javascript_driver
+  WebMock.disable! if defined? WebMock
+end
+
+>>>>>>> poltergeist for remote feature tests
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.filter_run_excluding :remote unless remote
+  #config.filter_run_excluding :js => false if remote
 
   config.order = 'random'
 end
+
 
 def form_date field, date
   {
@@ -24,6 +46,7 @@ def form_date field, date
     "#{field}(1i)" => date.try(:year)
   }
 end
+<<<<<<< HEAD
 
 def values_from_pdf file
   fields = `pdftk #{file} dump_data_fields`
@@ -34,3 +57,5 @@ def values_from_pdf file
     hash[field] = value if field.present?
   end
 end
+=======
+>>>>>>> poltergeist for remote feature tests
