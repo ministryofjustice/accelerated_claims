ab48d2f8655864c2c6e79d342cfdcb24ec73be62
Tom Gladhill <whoojemaflip@gmail.com>
2014-03-27 10:42:47 +0000

    download PDF works across environments

--- spec/support/page_models/confirmation_page.rb
+++ spec/support/page_models/confirmation_page.rb
@@ -14,53 +14,50 @@ class ConfirmationPage
 
   def download_pdf
 
-      # not all Capybara drivers implement these methods
-      # if page.respond_to? :evaluate_script && page.respond_to? :response_headers
-      #   page.evaluate_script("window.jQuery('a.pdf-download').removeAttr('target')")
-      #   click_link 'Print completed form'
-      #   expect(page.response_headers['Content-Type']).to eq "application/pdf"
-      # end
 
-      cookie_id = '_accelerated_claims_session'
-      session_id = get_me_the_cookie(cookie_id)[:value]
-
-      url = Capybara.app_host.blank? ? "http://localhost:#{Capybara.server_port}" : Capybara.app_host
-      url += "/download"
-
-      http = Curl.get(url) do |http|
-        http.headers['Cookie'] = "#{cookie_id}=#{session_id}"
-        http.ssl_verify_peer = false
-        http.ssl_verify_host = false
-        http.follow_location = true
-      end
-
-      response_headers = parse_headers(http.header_str)
-      expect(response_headers['Content-Type']).to eq "application/pdf"
-
-      file = Tempfile.new('pdf_download')
-      file.write(http.body_str)
-
-      file.path
-
-
-
-      # todo: use curl to download the PDF
-      # parse the PDF
-      # assertions
-
-
-      # generated_file = '/tmp/a.pdf'
-      # File.open(generated_file, 'w') { |file| file.write(page.body.encode("ASCII-8BIT").force_encoding("UTF-8")) }
-
-      # generated_values = values_from_pdf generated_file
-
-      # expected_values.each do |field, value|
-      #   "#{field}: #{generated_values[field]}".should == "#{field}: #{value}"
-      # end
+    pdf_filename = case Capybara.app_host.blank?
+    when true
+      capybara_download_pdf
+    else
+      curl_download_pdf
+    end
 
   end
 
 private
+  def capybara_download_pdf
+    expect(Capybara.current_path).to eql "/confirmation"
+    click_link 'Print completed form'
+    assert_pdf_content_type(page.response_headers)
+    
+    write_pdf_to_tempfile page.body
+  end
+
+  def curl_download_pdf
+    cookie_id = '_accelerated_claims_session'
+    session_id = get_me_the_cookie(cookie_id)[:value]
+
+    http = Curl.get(Capybara.app_host + "/download") do |http|
+      http.headers['Cookie'] = "#{cookie_id}=#{session_id}"
+      http.ssl_verify_peer = false
+      http.ssl_verify_host = false
+      http.follow_location = true
+    end
+
+    assert_pdf_content_type(parse_headers http.header_str)
+    write_pdf_to_tempfile http.body_str
+  end
+
+  def write_pdf_to_tempfile(ascii)
+    file = Tempfile.new('pdf_download', encoding: 'utf-8')
+    file.write(ascii.encode("ASCII-8BIT").force_encoding("UTF-8"))
+    file.path
+  end
+
+  def assert_pdf_content_type(headers_hash)
+    expect(headers_hash['Content-Type']).to eq "application/pdf"
+  end
+
   def parse_headers(curl_header_string)
     http_response, *http_headers = curl_header_string.split(/[\r\n]+/).map(&:strip)
     http_headers = Hash[http_headers.flat_map{ |s| s.scan(/^(\S+): (.+)/) }]
