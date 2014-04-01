c62ec3c1ceeafbdfefbeb922c6cb1e38ba390ffc
Rob McKinnon <@digital.justice.gov.uk>
2014-04-01 17:34:38 +0100

    added 3 new scenarios - taken from deleted new_claim_spec.rb

---
+++ spec/fixtures/scenario_10_data.rb
@@ -0,0 +1,98 @@
+{
+  title: "JOURNEY 10",
+  description: [
+    "base test",
+    "2 claimants, not demoted tenancy"
+  ],
+  claim: {
+    form_state: "",
+    property: {
+      street: "Mucho Gracias Road",
+      town: "London",
+      postcode: "SW1H 9AJ",
+      house: "Yes"
+    },
+    claimant_one: {
+      title: "Mr",
+      full_name: "John Smith",
+      street: "2 Brown St",
+      town: "Cwmbran",
+      postcode: "SW1W 0LU"
+    },
+    claimant_two: {
+      title: "Ms",
+      full_name: "Jane Smith",
+      street: "7 Main St",
+      town: "Alfreton",
+      postcode: "SW1W 0LU"
+    },
+    claimant_contact: {
+      legal_costs: "123.34",
+      title: "Mr",
+      full_name: "Jim Brown",
+      company_name: "",
+      street: "3 Smith St",
+      town: "Winsum",
+      postcode: "SW1W 0LU",
+      email: "jim@example.com",
+      phone: "020 000 000",
+      fax: "020 000 000",
+      dx_number: "DX 123",
+      reference_number: "my-ref-123"
+    },
+    defendant_one: {
+      title: "Mr",
+      full_name: "John Major",
+      street: "Sesame Street",
+      town: "London",
+      postcode: "SW1X 2PT"
+    },
+    defendant_two: {
+      title: "",
+      full_name: "",
+      street: "",
+      town: "",
+      postcode: ""
+    },
+    demoted_tenancy: {
+      demoted_tenancy: "No",
+      demotion_order_date: nil,
+      demotion_order_court: ""
+    },
+    tenancy: {
+      start_date: "2010-1-1",
+      latest_agreement_date: "2010-1-1",
+      reissued_for_same_property: "Yes",
+      reissued_for_same_landlord_and_tenant: "Yes",
+      assured_shorthold_tenancy_notice_served_by: "Mr Brown",
+      assured_shorthold_tenancy_notice_served_date: "2013-12-1"
+    },
+    notice: {
+      served_by: "Somebody",
+      date_served: "2014-2-2",
+      expiry_date: "2014-2-2"
+    },
+    license: {
+      multiple_occupation: "Yes",
+      issued_under_act_part: "Part2",
+      issued_by: "Great authority",
+      issued_date: "2014-2-2"
+    },
+    deposit: {
+      received: "Yes",
+      demotion_order_date: nil,
+      ref_number: "X1234",
+      as_property: "Yes"
+    },
+    order: {
+      possession: "Yes",
+      cost: "Yes"
+    },
+    possession: {
+      hearing: "Yes"
+    },
+    fee: {
+      court_fee: "175.00"
+    }
+  }
+}
