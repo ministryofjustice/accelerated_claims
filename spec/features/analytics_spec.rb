e6d3a26e61cccb86d124d248e9d1a586337b0717
Tom Gladhill <whoojemaflip@gmail.com>
2014-04-03 14:54:24 +0100

    Tests for analytics cookies

--- spec/features/analytics_spec.rb
+++ spec/features/analytics_spec.rb
@@ -1,6 +1,15 @@
 feature 'analytics configuration', :remote => true do
-  scenario 'piwik cookies are set' do
+  scenario 'Piwik analytics cookies are set' do
     visit '/'
-    expect(get_me_the_cookie '_pk_id').to_not be_nil
+    found_piwik_id_cookie = false
+    get_me_the_cookies.each do |cookie|
+      found_piwik_id_cookie = true if(cookie[:name] =~ /^_pk_id/)
+    end
+    expect(found_piwik_id_cookie).to be_true
+  end
+
+  scenario 'Google analytics cookies are set' do
+    visit '/'
+    expect(get_me_the_cookie '_ga').to_not be_nil
   end
 end
