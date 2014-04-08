41bf9accb71a24450bf8ce752fe6017922053d18
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-04-08 15:42:59 +0100

    Removed left over setter & getter in Tenancy
    They were there in order to get certain specs passing.
    But they should not be there any longer.

--- spec/models/claim_spec.rb
+++ spec/models/claim_spec.rb
@@ -49,7 +49,7 @@ describe Claim do
       end
 
       it 'should set demoted tenancy boolean on tenancy' do
-        claim.tenancy.demoted_tenancy.should be_true
+        claim.tenancy.demoted_tenancy?.should be_true
       end
     end
 
