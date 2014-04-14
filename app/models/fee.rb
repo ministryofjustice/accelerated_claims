3b9006e606dee1dec79e8dbb4a428bc92e77c7b4
Rob McKinnon <@digital.justice.gov.uk>
2014-04-14 16:12:27 +0100

    additional length validation

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,7 +1,7 @@
 class Fee < BaseClass
 
   attr_accessor :court_fee
-  validates :court_fee, presence: { message: 'must be entered' }
+  validates :court_fee, presence: { message: 'must be entered' }, length: { maximum: 8 }
 
   def court_fee
     "175.00"
