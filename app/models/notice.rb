fed299881ae208cdead913ba1cf7c323dedf1a12
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-04-29 14:18:39 +0100

    Notice#served_by split into two form fields
    This was done as part of the copy changes drive

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,7 +1,10 @@
 class Notice < BaseClass
 
-  attr_accessor :served_by
-  validates :served_by, presence: { message: 'must be entered' }, length: { maximum: 70 }
+  attr_accessor :served_by_name
+  validates :served_by_name, presence: { message: 'must be entered' }, length: { maximum: 70 }
+
+  attr_accessor :served_method
+  validates :served_method, presence: { message: 'must be entered' }, length: { maximum: 35 }
 
   attr_accessor :date_served
   validates :date_served, presence: { message: 'must be entered' }
@@ -13,6 +16,8 @@ class Notice < BaseClass
 
   def as_json
     json = super
+    json["served_by"] = "#{json["served_by_name"]}, #{json["served_method"]}"
+    ["served_by_name", "served_method"].each { |attr| json.delete attr }
     json = split_date :date_served, json
     json = split_date :expiry_date, json
     json
--- spec/models/notice_spec.rb
+++ spec/models/notice_spec.rb
@@ -1,15 +1,15 @@
 describe Notice do
   let(:notice) do
-    Notice.new(served_by: "Jim Bob",
+    Notice.new(served_by_name: "Jim Bob",
+               served_method: "by post",
                date_served: Date.parse("2013-01-01"),
-               expiry_date: Date.parse("2014-02-02")
-               )
+               expiry_date: Date.parse("2014-02-02"))
   end
 
   describe "#as_json" do
     let(:desired_format) do
       {
-        "served_by" => "Jim Bob",
+        "served_by" => "Jim Bob, by post",
         "date_served_day" => "01",
         "date_served_month" => "01",
         "date_served_year" => "2013",
@@ -30,14 +30,26 @@ describe Notice do
     end
   end
 
-  describe "served_by" do
+  describe "served_by_name" do
     it "should be required" do
-      notice.served_by = ""
+      notice.served_by_name = ""
       notice.should_not be_valid
     end
 
     it "should be under 70 characters" do
-      notice.served_by = "x" * 71
+      notice.served_by_name = "x" * 71
+      notice.should_not be_valid
+    end
+  end
+
+  describe "served_method" do
+    it "should be required" do
+      notice.served_method = ""
+      notice.should_not be_valid
+    end
+
+    it "should be under 35 characters" do
+      notice.served_method = "x" * 36
       notice.should_not be_valid
     end
   end
