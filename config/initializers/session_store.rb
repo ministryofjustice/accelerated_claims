5b1013d2ec3114531275929827e33bbadf9e8d7d
Rob McKinnon <@digital.justice.gov.uk>
2014-04-29 11:56:57 +0100

    set session_store expire_after to 30 mins

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-AcceleratedClaims::Application.config.session_store :cache_store, key: '_accelerated_claims_session'
+AcceleratedClaims::Application.config.session_store :cache_store,
+    key: '_accelerated_claims_session',
+    expire_after: 30.minutes
