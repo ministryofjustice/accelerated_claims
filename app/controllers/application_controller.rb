2909edd35af3d704608da2a4dd3fb93a82e9fa85
Rob McKinnon <@digital.justice.gov.uk>
2014-04-29 14:30:14 +0100

    comment out protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   def redirect_to_with_protocol action, options={}
     redirect_to url_for( options.merge(action: action, protocol: (Rails.env.production? ? 'https' : 'http') ) )
