5ac00f100159f5037049bcffed0bbfb47ab02e21
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-04-29 17:34:20 +0100

    WIP

--- app/assets/javascripts/tenancy_module.js
+++ app/assets/javascripts/tenancy_module.js
@@ -7,19 +7,52 @@ moj.Modules.tenancyModule = (function() {
   var //functions
       init,
       cacheEls,
+      bindEvents,
+      checkDate,
 
       //elements
-      $div
+      $optionsDiv,
+      $datepickers,
+
+      //data
+      dates = {
+        first:    '1989-01-15',
+        second:   '1997-02-28'
+      }
       ;
 
   init = function() {
     cacheEls();
+    bindEvents();
 
-    $div.addClass( 'inset' ).find( '.inset' ).removeClass( 'inset' ).removeClass( 'sub-panel' );
+    $optionsDiv.addClass( 'inset' ).find( '.inset' ).removeClass( 'inset' ).removeClass( 'sub-panel' );
+
+
   };
 
   cacheEls = function() {
-    $div = $( '.assuredTenancyOptions' ).eq( 0 );
+    $optionsDiv = $( '.assuredTenancyOptions' ).eq( 0 );
+    $datepickers = $( '.date-picker.conditional' );
+  };
+
+  bindEvents = function() {
+    $datepickers.each( function() {
+      var $this = $( this );
+      $this.find( 'select' ).on( 'change', function() {
+        checkDate( $this );
+      } );
+    } );
+  };
+
+  checkDate = function( $fs ) {
+    var day = $fs.find( '.day' ).val(),
+        month = $fs.find( '.month' ).val(),
+        year = $fs.find( '.year' ).val();
+
+
+    if( day && month && year ) {
+      moj.log(day + '-' + month + '-' + year);
+    }
   };
 
   // public
