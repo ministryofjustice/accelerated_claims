078320e61af52ba010efd11e4d382cbf8d5dfc5f
Rob McKinnon <@digital.justice.gov.uk>
2014-05-02 17:21:54 +0100

    fix param filtering

--- config/initializers/parameter_whitelisting.rb
+++ config/initializers/parameter_whitelisting.rb
@@ -3,6 +3,6 @@ SANITIZED_VALUE = '[FILTERED]'.freeze
 
 AcceleratedClaims::Application.config.filter_parameters << lambda do |key, value|
   unless key.match(WHITELISTED_KEYS_MATCHER)
-    value.replace(SANITIZED_VALUE)
+    value ? value.replace(SANITIZED_VALUE) : ''
   end
 end
