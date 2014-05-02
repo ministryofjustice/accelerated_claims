85a00f70381539a421189894e0f9765d2b640601
Rob McKinnon <@digital.justice.gov.uk>
2014-05-02 14:24:25 +0100

    update feature for JS validation

--- spec/features/form_completion_spec.rb
+++ spec/features/form_completion_spec.rb
@@ -1,6 +1,10 @@
 feature 'Filling in claim form' do
 
-  scenario "submitting incomplete form" do
+  before do
+    WebMock.disable_net_connect!(:allow => "127.0.0.1")
+  end
+
+  scenario "submitting incomplete form", js: false do
     visit '/new'
     click_button 'Complete form'
     expect(page).to have_content("Street must be entered")
@@ -10,6 +14,14 @@ feature 'Filling in claim form' do
     expect(page).to have_content("Served method must be entered")
 
     expect(page).to have_content("House must be selected")
+  end
+
+  scenario "submitting incomplete form", js: true do
+    visit '/new'
+    click_button 'Complete form'
+
+    expect(page).to have_content(%Q|Question "As the landlord, you're known as the claimant in this case. How many claimants are there?" not answered|)
+    expect(page).to have_content(%Q|Question "Your tenants are known as defendants in this case. How many defendants are there?" not answered|)
   end
 
 end
