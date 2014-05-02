c1231282062865cd3d5a93d97a61d6858ad7f675
Rob McKinnon <@digital.justice.gov.uk>
2014-05-02 15:01:12 +0100

    prevent js false feature running when remote testing

--- spec/features/form_completion_spec.rb
+++ spec/features/form_completion_spec.rb
@@ -4,16 +4,18 @@ feature 'Filling in claim form' do
     WebMock.disable_net_connect!(:allow => "127.0.0.1")
   end
 
-  scenario "submitting incomplete form", js: false do
-    visit '/new'
-    click_button 'Complete form'
-    expect(page).to have_content("Street must be entered")
+  unless remote_test?
+    scenario "submitting incomplete form", js: false do
+      visit '/new'
+      click_button 'Complete form'
+      expect(page).to have_content("Street must be entered")
 
-    expect(page).to have_content("Served by name must be entered")
+      expect(page).to have_content("Served by name must be entered")
 
-    expect(page).to have_content("Served method must be entered")
+      expect(page).to have_content("Served method must be entered")
 
-    expect(page).to have_content("House must be selected")
+      expect(page).to have_content("House must be selected")
+    end
   end
 
   scenario "submitting incomplete form", js: true do
