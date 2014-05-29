feb2c43945ab84378f610dc160aeb7c9b319d80b
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-05-29 15:47:39 +0100

    Workaround for when the attributes are present
    In some cases an attribute is present but all it contains is the
    newline. This is a workaround not create an extra count when it isn't
    required.

--- app/lib/document_count.rb
+++ app/lib/document_count.rb
@@ -6,9 +6,16 @@ class DocumentCount
 
   def add
     counter = 0
-    (@json.key? 'defendant_two_address') ? (counter += 2) : (counter += 1)
-    (@json.key? 'claimant_two_address') ? (counter += 2) : (counter += 1)
+    (blank_and_present? 'defendant_two_address') ? (counter += 2) : (counter += 1)
+    (blank_and_present? 'claimant_two_address') ? (counter += 2) : (counter += 1)
     @json['number-copies'] = (counter += 1)
     @json
+  end
+
+  private
+
+  def blank_and_present? attr
+    return false unless (@json.key? attr)
+    !@json["#{attr}"].gsub('\n', '').blank?
   end
 end
