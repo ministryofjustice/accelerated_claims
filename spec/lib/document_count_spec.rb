c1684b33b7fe4b2370baf1c3d326d2acc3e0fc76
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-05-29 18:07:09 +0100

    Renamed one of the fields in PDF, added tests
    Renamed 'number-copies' to 'copy_number' so that it can be used as a key
    in Ruby hash (the hyphen was the issue).
    
    Modified the test so that they check for the correct value of
    'copy_number'.

--- spec/lib/document_count_spec.rb
+++ spec/lib/document_count_spec.rb
@@ -6,7 +6,7 @@ describe DocumentCount do
       let(:count) { DocumentCount.new(json).add }
 
       it 'should add the number of documents to JSON' do
-        count["number-copies"].should eq 5
+        count["copy_number"].should eq 5
       end
     end
 
@@ -19,7 +19,7 @@ describe DocumentCount do
       let(:count) { DocumentCount.new(json).add }
 
       it 'should add the number of documents to JSON' do
-        count["number-copies"].should eq 4
+        count["copy_number"].should eq 4
       end
     end
 
@@ -32,7 +32,7 @@ describe DocumentCount do
       let(:count) { DocumentCount.new(json).add }
 
       it 'should add the number of documents to JSON' do
-        count["number-copies"].should eq 4
+        count["copy_number"].should eq 4
       end
     end
 
@@ -46,7 +46,7 @@ describe DocumentCount do
       let(:count) { DocumentCount.new(json).add }
 
       it 'should add the number of documents to JSON' do
-        count["number-copies"].should eq 3
+        count["copy_number"].should eq 3
       end
     end
   end
