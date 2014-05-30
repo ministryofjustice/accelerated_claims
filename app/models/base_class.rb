0d7c5902d063f6185f28295d10882304ed92ea13
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-05-30 17:10:42 +0100

    validate dates to be later than 15/1/1989 and not later than today, and make error message more meaningful

--- app/models/base_class.rb
+++ app/models/base_class.rb
@@ -102,7 +102,7 @@ class BaseClass
 end
 
 class DateValidator < ActiveModel::Validator
-  @@earliest_possible_date = Date.new(1989, 1, 1)
+  @@earliest_possible_date = Date.new(1989, 1, 15)
   def validate(record)
     options[:fields].each do |field|
       date = record.send(field)
@@ -112,7 +112,7 @@ class DateValidator < ActiveModel::Validator
       elsif date.blank?
         record.errors.add(field, 'cannot be blank')
       elsif date < @@earliest_possible_date
-        record.errors.add(field, 'cannot be before 01 Jan 1989')
+        record.errors.add(field, 'Incorrect date: you can only use this form with an assured shorthold tenancy (introduced 15 January 1989)')
       elsif date > Date.today
         record.errors.add(field, 'cannot be later than current date')
       end
