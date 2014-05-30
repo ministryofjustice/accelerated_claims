164d054391ea57fa5426464fe6df0bafd5d58a74
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-05-30 09:49:39 +0100

    Slimmed down document counting method
    By using 'present?' method I've shed a lot unneeded code.

--- app/lib/document_count.rb
+++ app/lib/document_count.rb
@@ -6,16 +6,9 @@ class DocumentCount
 
   def add
     counter = 0
-    (blank_and_present? 'defendant_two_address') ? (counter += 2) : (counter += 1)
-    (blank_and_present? 'claimant_two_address') ? (counter += 2) : (counter += 1)
+    @json['defendant_two_address'].present? ? (counter += 2) : (counter += 1)
+    @json['claimant_two_address'].present? ? (counter += 2) : (counter += 1)
     @json['copy_number'] = (counter += 1)
     @json
-  end
-
-  private
-
-  def blank_and_present? attr
-    return false unless (@json.key? attr)
-    !@json["#{attr}"].gsub('\n', '').blank?
   end
 end
