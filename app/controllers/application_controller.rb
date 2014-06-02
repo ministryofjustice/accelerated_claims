a23ca3547efa8763c94e46f937df09e3a2e17034
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-06-02 11:53:43 +0100

    Enabling server startup in all environments except production without Cross-Site Request Forgery protection if CC-NO_CSRF environment variable is set (used for load testing).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # For APIs, you may want to use :null session instead.
+  if Rails.env.production? || ENV['CC_NO_CSRF'] != '1'
+    protect_from_forgery with: :exception
+  else
 
   def redirect_to_with_protocol action, options={}
     redirect_to url_for( options.merge(action: action, protocol: (Rails.env.production? ? 'https' : 'http') ) )
