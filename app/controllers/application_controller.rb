c374ac65fe022a6fc45def46b4903edf161f6301
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-06-02 11:57:46 +0100

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null session instead.
   if Rails.env.production? || ENV['CC_NO_CSRF'] != '1'
     protect_from_forgery with: :exception
-  else
+  end
 
   def redirect_to_with_protocol action, options={}
     redirect_to url_for( options.merge(action: action, protocol: (Rails.env.production? ? 'https' : 'http') ) )
