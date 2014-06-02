65ce633471e010dd6748e32da69f1b76f31d12da
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-06-02 10:40:14 +0100

    Move InvalidDate class into its own file

--- app/models/base_class.rb
+++ app/models/base_class.rb
@@ -120,15 +120,4 @@ class DateValidator < ActiveModel::Validator
   end
 end
 
-class InvalidDate
 
-  attr_reader :day, :long_monthname, :year
-
-  def initialize(date_string)
-    parts = date_string.split('-')
-    @day = parts[2]
-    @long_monthname = parts[1]
-    @year = parts[0]
-  end
-  
-end
