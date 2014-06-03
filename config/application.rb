d7e6006f707dc5b5a8fa6b8271b589483faa3f7d
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-06-03 17:58:40 +0100

    Revisited autoloading of files under 'app/lib'

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module AcceleratedClaims
     # Use 'auto_add_path' for it to add a path link to the new_feedback route
     config.feedback_url = config.relative_url_root + '/feedback/new'
 
-    config.autoload_paths += %W(#{Rails.root}/app/lib/checklist #{Rails.root}/app/lib)
+    config.autoload_paths += Dir["#{Rails.root}/app/lib/**/"]
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -68,4 +68,3 @@ end
 
 puts "Rails env: #{ENV['RAILS_ENV']}"
 puts "Rack env: #{ENV['RACK_ENV']}"
-
