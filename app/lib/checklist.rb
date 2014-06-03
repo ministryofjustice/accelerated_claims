7c7fca9e7d90ff8e05a1c3e050daef50663af670
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-06-03 15:14:06 +0100

    Deposit checklist added

--- app/lib/checklist.rb
+++ app/lib/checklist.rb
@@ -1,3 +1,4 @@
 require 'checklist/tenancy_checklist'
 require 'checklist/notice_checklist'
 require 'checklist/license_checklist'
+require 'checklist/deposit_checklist'
---
+++ spec/lib/checklist/deposit_checklist_spec.rb
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+describe DepositChecklist do
+  describe '#add' do
+    let(:json) do
+      data = claim_formatted_data
+      data['required_documents'] = ''
+      data['deposit_received'] = 'Yes'
+      data['deposit_as_money'] = 'Yes'
+      data
+    end
+    let(:deposit) { DepositChecklist.new(json).add['required_documents'] }
+    let(:text) { "The tenancy deposit scheme certificate - marked 'F'" }
+
+    context 'when money deposit is given' do
+      it { expect(deposit).to eq text }
+    end
+  end
+end
