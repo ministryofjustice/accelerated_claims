7cabbc1bac4d81e09551d76d7c10aeb52e00aedd
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-06-03 15:40:37 +0100

    Full Checklist put together
    Unification of all the checklist models under one overarching checklist.

--- app/lib/checklist.rb
+++ app/lib/checklist.rb
@@ -2,3 +2,34 @@ require 'checklist/tenancy_checklist'
 require 'checklist/notice_checklist'
 require 'checklist/license_checklist'
 require 'checklist/deposit_checklist'
+
+class Checklist
+  def initialize(json)
+    @json = json
+  end
+
+  def add
+    add_tenancy_checklist
+    add_notice_checklist
+    add_license_checklist
+    add_deposit_checklist
+  end
+
+  private
+
+  def add_tenancy_checklist
+    TenancyChecklist.new(@json).add
+  end
+
+  def add_notice_checklist
+    NoticeChecklist.new(@json).add
+  end
+
+  def add_license_checklist
+    LicenseChecklist.new(@json).add
+  end
+
+  def add_deposit_checklist
+    DepositChecklist.new(@json).add
+  end
+end
