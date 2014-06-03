5349ecb6d898c9ecb8be1aedb3234a0f100dd9a9
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-06-03 10:06:46 +0100

    Add validation to ensure date notice expires is after date notice served

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -14,6 +14,8 @@ class Notice < BaseClass
 
   validates_with DateValidator, :fields => [:date_served, :expiry_date]
 
+  validate :expiry_date_must_be_after_served_date
+
   def as_json
     json = super
     json["served_by"] = "#{json["served_by_name"]}, #{json["served_method"]}"
@@ -21,5 +23,15 @@ class Notice < BaseClass
     json = split_date :date_served, json
     json = split_date :expiry_date, json
     json
+  end
+end
+
+
+
+def expiry_date_must_be_after_served_date
+  if date_served.is_a?(Date) && expiry_date.is_a?(Date)
+    unless expiry_date > date_served
+      errors.add(:expiry_date, "must be later than the Date Notice Served ")
+    end
   end
 end
