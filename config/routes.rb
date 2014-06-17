40c9f568c0d472b50db65f72d8c8f29a26f17f4b
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-06-17 14:13:45 +0100

    add expire_session action to application_controller.  if param redirect=false is appended, then no redirection to root_path takes place

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,16 @@ AcceleratedClaims::Application.routes.draw do
   scope AcceleratedClaims::Application.config.relative_url_root || '/' do
     root 'claim#new'
 
-    get  '/',              controller: :claim, action: :new
-    post '/submission',    controller: :claim, action: :submission
-    get  '/confirmation',  controller: :claim, action: :confirmation
-    get  '/download',      controller: :claim, action: :download
+    get  '/',               controller: :claim, action: :new
+    post '/submission',     controller: :claim, action: :submission
+    get  '/confirmation',   controller: :claim, action: :confirmation
+    get  '/download',       controller: :claim, action: :download
 
-    get  '/cookies',       controller: :static, action: :cookies
-    get  '/terms',         controller: :static, action: :terms
+    get  '/cookies',        controller: :static, action: :cookies
+    get  '/terms',          controller: :static, action: :terms
 
-    get  '/heartbeat',     controller: :application, action: :heartbeat
+    get  '/heartbeat',      controller: :application, action: :heartbeat
+    get  '/expire_session', controller: :application, action: :expire_session
 
     # zendesk
     resource :feedback,   only: [:new, :create], controller: 'feedback'
