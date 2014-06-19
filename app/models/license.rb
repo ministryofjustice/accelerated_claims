288a0d66ac9fd98a644bbadc5999ab04ccb45ae9
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-06-19 10:27:24 +0100

    Licence refactoring to take into account applied for

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,25 +1,42 @@
 class License < BaseClass
 
   attr_accessor :multiple_occupation
-  attr_accessor :issued_under_act_part
+  attr_accessor :issued_under_act_part_yes
+  attr_accessor :issued_under_act_part_applied
   attr_accessor :issued_by
   attr_accessor :issued_date
 
   validates :multiple_occupation, presence: { message: 'must be selected' }, inclusion: { in: ['Yes', 'No', 'Applied'] }
 
   with_options if: :in_multiple_occupation? do |license|
-    license.validates :issued_under_act_part, presence: { message: 'must be selected' }, inclusion: { in: ['Part2', 'Part3'] }
+    license.validates :issued_under_act_part_yes, presence: { message: 'must be selected' }, inclusion: { in: ['Part2', 'Part3'] }
     license.validates :issued_by, presence: { message: "can't be blank" }, length: { maximum: 70 }
     license.validates :issued_date, presence: { message: "can't be blank" }
     validates_with DateValidator, :fields => [:issued_date]
   end
 
   with_options if: :hmo_licence_applied_for? do |license|
-    license.validates :issued_under_act_part, presence: { message: 'must be selected' }, inclusion: { in: ['Part2', 'Part3'] }
+    license.validates :issued_under_act_part_applied, presence: { message: 'must be selected' }, inclusion: { in: ['Part2', 'Part3'] }
     license.validates :issued_by, absence: { message: "Must be blank if HMO licence applied for" }
     license.validates :issued_date, absence: { message: "Must be blank if HMO licence applied for" }
   end
 
+
+  # def issued_under_act_part_yes
+  #   in_multiple_occupation?
+  # end
+
+  # def issued_under_act_part_yes=(value)
+  #   multiple_occupation = 'Yes'
+  # end
+
+  # def issued_under_act_part_applied
+  #   hmo_licence_applied_for?
+  # end
+
+  # def issued_under_act_part_applied=(value)
+  #   multiple_occupation = 'Applied'
+  # end
 
 
   def in_multiple_occupation?
@@ -45,8 +62,8 @@ class License < BaseClass
       "part3_year" => ''
     }
 
-    if in_multiple_occupation? || hmo_licence_applied_for?
-      case issued_under_act_part
+    if in_multiple_occupation? 
+      case issued_under_act_part_yes
       when 'Part2'
         default_values.merge({
           "multiple_occupation" => 'Yes',
@@ -63,6 +80,26 @@ class License < BaseClass
           "part3_day" => day(issued_date),
           "part3_month" => month(issued_date),
           "part3_year" => year(issued_date)
+        })
+      end
+    elsif hmo_licence_applied_for?
+      case issued_under_act_part_applied
+      when 'Part2'
+        default_values.merge({
+          "multiple_occupation" => 'Yes',
+          "part2_authority" => '',
+          "part2_day" => '',
+          "part2_month" => '',
+          "part2_year" => ''
+        })
+      when 'Part3'
+        default_values.merge({
+          "multiple_occupation" => 'No',
+          "part3" => 'Yes',
+          "part3_authority" => '',
+          "part3_day" => '',
+          "part3_month" => '',
+          "part3_year" => ''
         })
       end
     else
--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -3,7 +3,8 @@ describe License do
   let(:license) do
     License.new({
         multiple_occupation: multiple_occupation,
-        issued_under_act_part: issued_under_act_part,
+        issued_under_act_part_yes: issued_under_act_part_yes,
+        issued_under_act_part_applied: issued_under_act_part_applied,
         issued_by: issued_by,
       }.merge(
         form_date 'issued_date', issued_date
@@ -14,50 +15,62 @@ describe License do
   subject { license }
 
   context 'licence applied for' do
-    let(:multiple_occupation) { 'Applied' }
-    let(:issued_by) { '' }
-    let(:issued_date) { '' }
+    let(:multiple_occupation)           { 'Applied' }
+    let(:issued_under_act_part_yes)     { '' }
+    let(:issued_by)                     { '' }
+    let(:issued_date)                   { '' }
 
     context 'no part 2 or part 3 selected' do 
-      let(:issued_under_act_part)     { '' }
+      let(:issued_under_act_part_applied) { '' }
       it { should_not be_valid }
     end
 
 
     context 'and licensed under part 2 of act' do
-      let(:issued_under_act_part) { 'Part2' }
+      let(:issued_under_act_part_applied)   { 'Part2' }
       it { should be_valid }
       its(:as_json) { should == {
           "multiple_occupation" => 'Yes',
-          "part2_authority" =>  '',
-          "part2_day" => '',
-          "part2_month" => '',
-          "part2_year" => '',
-          "part3" => 'No',
-          "part3_authority" => '',
-          "part3_day" => '',
-          "part3_month" => '',
-          "part3_year" => ''
+          "part2_authority"     => '',
+          "part2_day"           => '',
+          "part2_month"         => '',
+          "part2_year"          => '',
+          "part3"               => 'No',
+          "part3_authority"     => '',
+          "part3_day"           => '',
+          "part3_month"         => '',
+          "part3_year"          => ''
         }
       }
     end
 
     context 'and licensed under part 3 of act' do
-      let(:issued_under_act_part) { 'Part3' }
+      let(:issued_under_act_part_applied)   { 'Part3' }
       it { should be_valid }
       its(:as_json) { should == {
           "multiple_occupation" => 'No',
-          "part2_authority" =>  '',
-          "part2_day" => '',
-          "part2_month" => '',
-          "part2_year" => '',
-          "part3" => 'Yes',
-          "part3_authority" => '',
-          "part3_day" => '',
-          "part3_month" => '',
-          "part3_year" => ''
+          "part2_authority"     => '',
+          "part2_day"           => '',
+          "part2_month"         => '',
+          "part2_year"          => '',
+          "part3"               => 'Yes',
+          "part3_authority"     => '',
+          "part3_day"           => '',
+          "part3_month"         => '',
+          "part3_year"          => ''
         }
       }
+    end
+
+    context 'and issued_under_act_part_applied invalid' do
+      let(:issued_under_act_part_applied) { 'Part100' }
+      it { should_not be_valid }
+    end   
+
+
+    context 'and issued_under_act_part_applied blank' do
+      let(:issued_under_act_part_applied) { '' }
+      it { should_not be_valid }
     end
 
 
@@ -65,53 +78,57 @@ describe License do
 
 
   context 'in multiple occupation' do
-    let(:multiple_occupation) { 'Yes' }
-    let(:issued_by) { 'Westminster City' }
-    let(:issued_date) { Date.parse("2013-01-12") }
-    let(:issued_under_act_part) { 'Part2' }
+    let(:multiple_occupation)             { 'Yes' }
+    let(:issued_by)                       { 'Westminster City' }
+    let(:issued_date)                     { Date.parse("2013-01-12") }
+    let(:issued_under_act_part_applied)   { '' }
+
+    let(:issued_under_act_part_yes) { 'Part2' }
 
     context 'and licensed under part 2 of act' do
       it { should be_valid }
       its(:as_json) { should == {
           "multiple_occupation" => 'Yes',
-          "part2_authority" => issued_by,
-          "part2_day" => '12',
-          "part2_month" => '01',
-          "part2_year" => '2013',
-          "part3" => 'No',
-          "part3_authority" => '',
-          "part3_day" => '',
-          "part3_month" => '',
-          "part3_year" => ''
+          "part2_authority"     => issued_by,
+          "part2_day"           => '12',
+          "part2_month"         => '01',
+          "part2_year"          => '2013',
+          "part3"               => 'No',
+          "part3_authority"     => '',
+          "part3_day"           => '',
+          "part3_month"         => '',
+          "part3_year"          => ''
         }
       }
     end
 
     context 'and licensed under part 3 of act' do
-      let(:issued_under_act_part) { 'Part3' }
+      let(:issued_under_act_part_yes) { 'Part3' }
       it { should be_valid }
       its(:as_json) { should == {
           "multiple_occupation" => 'No',
-          "part2_authority" => '',
-          "part2_day" => '',
-          "part2_month" => '',
-          "part2_year" => '',
-          "part3" => 'Yes',
-          "part3_authority" => issued_by,
-          "part3_day" => '12',
-          "part3_month" => '01',
-          "part3_year" => '2013'
+          "part2_authority"     => '',
+          "part2_day"           => '',
+          "part2_month"         => '',
+          "part2_year"          => '',
+          "part3"               => 'Yes',
+          "part3_authority"     => issued_by,
+          "part3_day"           => '12',
+          "part3_month"         => '01',
+          "part3_year"          => '2013'
         }
       }
     end
 
-    context 'and issued_under_act_part invalid' do
-      let(:issued_under_act_part) { 'Part100' }
+    context 'and issued_under_act_part_yes invalid' do
+      let(:issued_under_act_part_yes) { 'Part100' }
       it { should_not be_valid }
     end
 
+ 
+
     context 'and issued_under_act_part blank' do
-      let(:issued_under_act_part) { '' }
+      let(:issued_under_act_part_yes) { '' }
       it { should_not be_valid }
     end
 
@@ -127,33 +144,35 @@ describe License do
   end
 
   context 'not in multiple occupation' do
-    let(:multiple_occupation) { 'No' }
-    let(:issued_date) { '' }
-    let(:issued_by) { '' }
-    let(:issued_under_act_part) { '' }
+    let(:multiple_occupation)             { 'No' }
+    let(:issued_date)                     { '' }
+    let(:issued_by)                       { '' }
+    let(:issued_under_act_part_yes)       { '' }
+    let(:issued_under_act_part_applied)   { '' }
 
     it { should be_valid }
 
     its(:as_json) { should == {
         "multiple_occupation" => 'No',
-        "part2_authority" => '',
-        "part2_day" => '',
-        "part2_month" => '',
-        "part2_year" => '',
-        "part3" => 'No',
-        "part3_authority" => '',
-        "part3_day" => '',
-        "part3_month" => '',
-        "part3_year" => ''
+        "part2_authority"     => '',
+        "part2_day"           => '',
+        "part2_month"         => '',
+        "part2_year"          => '',
+        "part3"               => 'No',
+        "part3_authority"     => '',
+        "part3_day"           => '',
+        "part3_month"         => '',
+        "part3_year"          => ''
       }
     }
   end
 
   context 'multiple occupation blank' do
-    let(:multiple_occupation) { '' }
-    let(:issued_date) { '' }
-    let(:issued_by) { '' }
-    let(:issued_under_act_part) { '' }
+    let(:multiple_occupation)               { '' }
+    let(:issued_date)                       { '' }
+    let(:issued_by)                         { '' }
+    let(:issued_under_act_part_yes)         { '' }
+    let(:issued_under_act_part_applied)     { '' }
 
     it { should_not be_valid }
 
