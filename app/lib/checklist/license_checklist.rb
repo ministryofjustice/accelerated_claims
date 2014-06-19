d5b53a237395709c03e7b97b5d9af02a6fbe1cf1
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-06-19 12:49:28 +0100

    Change checklist to handle HMO Licence applied for

--- app/lib/checklist/license_checklist.rb
+++ app/lib/checklist/license_checklist.rb
@@ -11,12 +11,12 @@ class LicenseChecklist
   private
 
   def add_license_text
-    if @json['license_multiple_occupation'] == 'Yes'
+    if @json['license_multiple_occupation'] == 'Applied'
       case @json['license_part3']
       when 'Yes'
         add_note(part3_text)
       when 'No'
-        authority_and_date_blank? ? add_note(part2_text) : ""
+        add_note(part2_text)
       else
         full_text if other_options_blank?
       end
@@ -28,37 +28,11 @@ class LicenseChecklist
   end
 
   def part2_text
-    "- Evidence of your HMO license application issued under part 2 of Housing Act 2004 - marked 'D'\n\n"
+    "- Evidence of your HMO license application issued under part 2 of the Housing Act 2004 - marked 'D'\n\n"
   end
 
   def part3_text
-    "- Evidence of your HMO license application issued under part 3 of Housing Act 2004 - marked 'E'\n\n"
+    "- Evidence of your HMO license application issued under part 3 of the Housing Act 2004 - marked 'E'\n\n"
   end
-
-  def full_text
-    @json['required_documents'] = @json['required_documents'].concat "- Evidence of your HMO license application
-
-- if issued under part 2 of Housing Act 2004 - marked 'D'
-- if issued under part 3 of Housing Act 2004 - marked 'E'\n\n"
-  end
-
-  def other_options_blank?
-    !@json['license_part2_authority'].present? &&
-      !@json['license_part2_day'].present? &&
-      !@json['license_part2_month'].present? &&
-      !@json['license_part2_year'].present? &&
-      !@json['license_part_year'].present? &&
-      !@json['part3'].present? &&
-      !@json['part3_authority'].present? &&
-      !@json['part3_day'].present? &&
-      !@json['part3_month'].present? &&
-      !@json['part3_year'].present?
-  end
-
-  def authority_and_date_blank?
-    !@json['license_part2_authority'].present? &&
-      !@json['license_part2_day'].present? &&
-      !@json['license_part2_month'].present? &&
-      !@json['license_part2_year'].present?
-  end
+  
 end
