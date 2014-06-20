d8aa082b20b12bf8cf20e419c81a9a7782b1993c
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-06-20 17:56:32 +0100

    Removed deprecated RSpec option
    RSpec::Core::Configuration#treat_symbols_as_metadata_keys_with_true_values=
    
    is deprecated, it is now set to true as default and setting it to false
    has no effect.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,6 @@ else
 end
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.filter_run_excluding :remote unless remote_test?
