df54b09f97dd8ddb46262b89568cefe654d6f927
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-06-23 09:47:57 +0100

    Added 'rspec-legacy_formatters' gem
    The reason for adding this gem is because some classes have 'puts'
    statements in them. Rather than removing them right now, while upgrading
    to RSpec 3, I'd leave them in and remove them later, when everything is
    working.
    
    Also removed the config that turns deprecations into exceptions.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,7 +31,6 @@ RSpec.configure do |config|
   config.include ShowMeTheCookies, type: :feature
 
   config.infer_spec_type_from_file_location!
-  config.raise_errors_for_deprecations!
 end
 
 def diffmerge(actual, expected)
