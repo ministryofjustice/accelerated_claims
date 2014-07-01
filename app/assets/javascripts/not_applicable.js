5410c4c4f1fba409b7d193718a45b55f2d807800
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-07-01 17:13:16 +0100

    add 'not applicable' radio buttons for nonjs users

---
+++ app/assets/javascripts/not_applicable.js
@@ -0,0 +1,40 @@
+/*jslint browser: true, evil: false, plusplus: true, white: true, indent: 2 */
+/*global moj, $ */
+
+moj.Modules.notApplicable = (function() {
+  "use strict";
+
+  var //functions
+      init,
+      cacheEls,
+      hideRadios,
+
+      //elements
+      $naRadios
+      ;
+
+  init = function() {
+    cacheEls();
+
+    hideRadios();
+  };
+
+  cacheEls = function() {
+    $naRadios = $( '[value="n/a"]' );
+  };
+
+  hideRadios = function() {
+    var $this;
+    $naRadios.each( function() {
+      $this = $( this );
+      $this.add( $this.parent( 'label' ) ).hide();
+    } );
+  };
+
+  // public
+
+  return {
+    init: init
+  };
+
+}());
