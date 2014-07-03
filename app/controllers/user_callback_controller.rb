9156ef39c8ff808bcae2be4538e9a6de122681d8
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-07-03 16:38:12 +0100

    Callback requests almost done
    The only thing that's outstanding is the way to properly send the custom
    data to Zendesk. The reason I didn't implement that is because I don't
    have access to our Zendesk instance. Once that's sorted, I'll be able
    create required custom fields, where the callback requests will be
    stored.

--- app/controllers/user_callback_controller.rb
+++ app/controllers/user_callback_controller.rb
@@ -1,6 +1,24 @@
+require 'zendesk_helper'
+
 class UserCallbackController < ApplicationController
 
   def new
+    @user_callback = UserCallback.new
   end
 
+  def create
+    @user_callback = UserCallback.new(user_callback_params)
+
+    if @user_callback.valid?
+      ZendeskHelper.callback_request(@user_callback)
+      redirect_to root_path, notice: "Thank you, we'll call you.", protocol: (Rails.env.production? ? 'https' : 'http')
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_callback_params
+    params.require(:user_callback).permit(:name, :phone, :description)
+  end
 end
--- spec/controllers/user_callback_controller_spec.rb
+++ spec/controllers/user_callback_controller_spec.rb
@@ -7,4 +7,19 @@ describe UserCallbackController, :type => :controller do
     end
   end
 
+  describe '#create' do
+    let(:callback_params) do
+      { name: 'Mike Smith', phone: '02011112222', description: 'Please call me!' }
+    end
+
+    it 'redirects to the homepage' do
+      post :create, user_callback: callback_params
+      expect(response).to redirect_to root_path
+    end
+
+    it 'sends feedback to zendesk' do
+      expect(ZendeskHelper).to receive(:callback_request).once
+      post :create, user_callback: callback_params
+    end
+  end
 end
