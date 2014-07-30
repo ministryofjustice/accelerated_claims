a7b076666c8de84fe835304141fe405b5eec4364
Rob McKinnon <@digital.justice.gov.uk>
2014-07-30 18:03:17 +0100

    added yes no question to start of notice section

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,5 +1,7 @@
 class Notice < BaseClass
 
+  attr_accessor :notice_served
+
   attr_accessor :served_by_name
   validates :served_by_name, presence: { message: 'Enter the name of the person who gave the notice' }, length: { maximum: 40 }
 
@@ -24,14 +26,13 @@ class Notice < BaseClass
     json = split_date :expiry_date, json
     json
   end
-end
 
-
-
-def expiry_date_must_be_after_served_date
-  if date_served.is_a?(Date) && expiry_date.is_a?(Date)
-    if expiry_date < (date_served + 2.months - 1.day)
-      errors.add(:expiry_date, "The date notice ended must be at least 2 months after the date notice served")
+  def expiry_date_must_be_after_served_date
+    if date_served.is_a?(Date) && expiry_date.is_a?(Date)
+      if expiry_date < (date_served + 2.months - 1.day)
+        errors.add(:expiry_date, "The date notice ended must be at least 2 months after the date notice served")
+      end
     end
   end
+
 end
