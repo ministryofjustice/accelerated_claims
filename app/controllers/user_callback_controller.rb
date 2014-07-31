cf11b7f0675b22159dc9a53f7c4734fe5af286d8
Rob McKinnon <@digital.justice.gov.uk>
2014-07-31 11:01:34 +0100

    unit tests for redirect from technical help form submission

--- app/controllers/user_callback_controller.rb
+++ app/controllers/user_callback_controller.rb
@@ -3,6 +3,7 @@ require 'zendesk_helper'
 class UserCallbackController < ApplicationController
 
   def new
+    session[:return_to] = request.referrer unless request.referrer.to_s[/#{feedback_path}|#{technical_help_path}/]
     @page_title = 'Make a claim to evict tenants - ask for technical help'
     @user_callback = UserCallback.new
   end
@@ -13,24 +14,19 @@ class UserCallbackController < ApplicationController
     if @user_callback.valid?
       ZendeskHelper.callback_request(@user_callback) unless @user_callback.test?
 
-      destination = set_destination_from session[:return_to]
       protocol = (Rails.env.production? ? 'https' : 'http')
       msg = 'Thank you we will call you back during the next working day between 9am and 5pm.'
 
-      redirect_to destination, notice: msg, protocol: protocol
+      redirect_to (session[:return_to] || '/'), notice: msg, protocol: protocol
     else
       render :new
     end
   end
 
   private
+
   def user_callback_params
     params.require(:user_callback).permit(:name, :phone, :description)
   end
 
-  def set_destination_from return_to
-    destination = return_to || '/'
-    destination = '/' if destination.match("#{feedback_path}|#{technical_help_path}")
-    destination
-  end
 end
--- spec/controllers/user_callback_controller_spec.rb
+++ spec/controllers/user_callback_controller_spec.rb
@@ -12,14 +12,72 @@ describe UserCallbackController, :type => :controller do
       { name: 'Mike Smith', phone: '02011112222', description: 'Please call me!' }
     end
 
-    it 'redirects to the homepage' do
-      post :create, user_callback: callback_params
-      expect(response).to redirect_to root_path
-    end
-
     it 'sends feedback to zendesk' do
       expect(ZendeskHelper).to receive(:callback_request).once
       post :create, user_callback: callback_params
     end
+
+    describe 'redirect' do
+      let(:referrer_path) { root_path }
+      let(:return_to) { nil }
+
+      before do
+        allow(controller.request).to receive(:referrer) { referrer_path }
+        session[:return_to] = return_to
+        get :new
+        post :create, user_callback: callback_params
+      end
+
+      context 'request referrer is homepage' do
+        it 'redirects to the homepage' do
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context 'request referrer is confirmation page' do
+        let(:referrer_path) { confirmation_path }
+
+        it 'redirects to confirmation page' do
+          expect(response).to redirect_to confirmation_path
+        end
+      end
+
+      context 'request referrer is feedback form' do
+        let(:referrer_path) { feedback_path }
+
+        context 'and session[:return_to] is nil' do
+          it 'redirects to homepage' do
+            expect(response).to redirect_to root_path
+          end
+        end
+
+        context 'and session[:return_to] is confirmation page' do
+          let(:return_to) { confirmation_path }
+
+          it 'redirects to confirmation page' do
+            expect(response).to redirect_to confirmation_path
+          end
+        end
+      end
+
+      context 'request referrer is technical help form' do
+        let(:referrer_path) { technical_help_path }
+
+        context 'and session[:return_to] is nil' do
+          it 'redirects to homepage' do
+            expect(response).to redirect_to root_path
+          end
+        end
+
+        context 'and session[:return_to] is confirmation page' do
+          let(:return_to) { confirmation_path }
+
+          it 'redirects to confirmation page' do
+            expect(response).to redirect_to confirmation_path
+          end
+        end
+      end
+    end
+
   end
 end
