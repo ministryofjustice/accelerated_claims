58e4a04dcfabe6db6ec882b33bb90d24657caa8e
Marcin <Cichon@Marcins-MacBook-Pro.local>
2014-08-11 12:26:20 +0100

    fixed the issue with number of claimant blocks not corresponding to selected number of claimants

--- app/assets/javascripts/multiple_persons.js
+++ app/assets/javascripts/multiple_persons.js
@@ -32,8 +32,13 @@ moj.Modules.multiplePersons = (function() {
   bindEvents = function() {
     $( document ).on( 'change', '.has-multiple .multiple [type="radio"]', function() {
       var $this = $( this );
-      showMultiples( $this.closest( '.has-multiple' ), $this, $this.val() );
-    } );
+      showMultiples($this.closest( '.has-multiple' ), $this, $this.val());
+    });
+
+    $('[name=claim\\[claimant_type\\]]').on('change', function(){
+      var $checked = $('[name=claim\\[num_claimants\\]]:checked');
+      showMultiples($(this).closest('.has-multiple'), $checked, $checked.val());
+    });
   };
 
   setupMultiples = function() {
