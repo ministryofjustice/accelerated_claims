7b01affed7c1638d0c9b88416c778ed23a34146a
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-08-11 17:14:33 +0100

    Added tests for claimant section
    The test try to prove claimant's details forms are displaying correctly
    when the user switches between private & commercial landlord types.

--- spec/features/claimant_spec.rb
+++ spec/features/claimant_spec.rb
@@ -2,12 +2,20 @@ feature 'Claimant' do
 
   before { WebMock.disable_net_connect!(:allow => "127.0.0.1") }
 
-  scenario '', js: true do
-    visit '/'
+  def switch_between_landlord_types
     choose 'A private landlord (individual)'
     choose '1 claimant'
     choose 'A private landlord (company), a local authority or a housing association'
     choose 'A private landlord (individual)'
+  end
+
+  def display_claimant_details_form_fields
     expect(page).to have_css('#claim_claimant_one_title')
+  end
+
+  scenario 'switching between landlord types', js: true do
+    visit '/'
+    switch_between_landlord_types
+    display_claimant_details_form_fields
   end
 end
