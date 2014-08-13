8d552e3a74563120d322034d774683f4c3c70362
Rob McKinnon <@digital.justice.gov.uk>
2014-08-13 11:09:52 +0100

    link to top of read-statements section for all errors in that section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,8 @@ module ApplicationHelper
     case field
     when /claim_tenancy_confirmed_(first|second)_rules_period_applicable_statements/
       'read-statements'
+    when /claim_tenancy_assured_shorthold_tenancy_notice_served_(by|date)_error/
+      'read-statements'
     when /claim_claimant_number_of_claimants_error/
       'claim_num_claimants_error'
     when /claim_defendant_number_of_defendants_error/
