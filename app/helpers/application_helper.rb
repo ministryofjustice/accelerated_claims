130aba90f77920357dabfc4ac2ce8f23eb26f7e2
Rob McKinnon <@digital.justice.gov.uk>
2014-08-13 10:14:14 +0100

    extracted link reference logic to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
 
+  def link_reference_for_error field
+    case field
+    when /claim_tenancy_confirmed_(first|second)_rules_period_applicable_statements/
+      'read-statements'
+    when /claim_claimant_number_of_claimants_error/
+      'claim_num_claimants_error'
+    when /claim_defendant_number_of_defendants_error/
+      'claim_num_defendants_error'
+    else
+      field
+    end
+  end
 
 end
