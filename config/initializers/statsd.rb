2a132b7a97003f9ba54daafd6b2f34a5611945aa
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-08-21 13:14:46 +0100

    Implemented the stats collection for statsd
    Reorganised the methods and made the obvious what their functionality is.

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,15 +1,34 @@
 $statsd = Statsd.new 'localhost', 8125
 
-events_to_measure = ['generate.pdf',
-                     'add_defendant_two.pdf',
-                     'strike_out_statements.pdf',
-                     'add_strikes_service.pdf',
-                     'store_strikes.pdf']
+pdf_events_to_measure = ['generate.pdf',
+                         'fill_form.pdf',
+                         'add_defendant_two.pdf',
+                         'strike_out_statements.pdf',
+                         'add_strikes_via_cli.pdf',
+                         'add_strikes_via_service.pdf',
+                         'error_add_strikes_commandline.pdf',
+                         'missing_file_add_strikes_commandline.pdf',
+                         'store_strikes.pdf']
 
-events_to_measure.each do |event|
+def reorder_namespace_for_statsd name
+  # performing this reordering as statsd's namespacing is from:
+  #   ---->
+  # left to right
+  #
+  # and ActiveSupport::Notifications' is
+  #   <----
+  # right to left
+  name.split('.').reverse.join('.')
+end
+
+def notify_of_duration name, start, finish
+  label = reorder_namespace_for_statsd name
+  duration_in_milliseconds = ((finish - start) * 1000).round
+  $statsd.timing "#{label}", duration_in_milliseconds
+end
+
+pdf_events_to_measure.each do |event|
   ActiveSupport::Notifications.subscribe(event) do |name, start, finish|
-    name_for_statsd = name.split('.').reverse.join('.')
-    duration_in_milliseconds = ((finish - start) * 1000).round
-    $statsd.timing "#{name_for_statsd}", duration_in_milliseconds
+    notify_of_duration name, start, finish
   end
 end
