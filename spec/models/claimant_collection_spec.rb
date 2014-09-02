b3dd480581c661fe9626c78c96e2453d95542b59
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-09-02 14:17:14 +0100

    claimant_collection_spec done

--- spec/models/claimant_collection_spec.rb
+++ spec/models/claimant_collection_spec.rb
@@ -1,6 +1,6 @@
 describe ClaimantCollection do
 
-  let(:cc)        { ClaimantCollection.new(claim_params) }
+  let(:cc)        { ClaimantCollection.new(test_claim_params) }
 
   describe '.new' do
     it 'should instantiate a collection with the correct number of claimants' do
@@ -9,7 +9,7 @@ describe ClaimantCollection do
     end
 
     it 'should fail if the number of claimants in the params is less than the number given in the initializer' do
-        params = claim_params
+        params = test_claim_params
         params[:num_claimants] = 4
         cc2 = ClaimantCollection.new(params)
         expect(cc2).not_to be_valid
@@ -28,7 +28,7 @@ describe ClaimantCollection do
     end
 
     it 'should insert claimant with validate absence = true if more than the num_claimants' do
-      params = claim_params
+      params = test_claim_params
       params['num_claimants'] = 2
       cc2 = ClaimantCollection.new(params)
       valid_claimant = cc2[1]
@@ -95,7 +95,7 @@ describe ClaimantCollection do
 
   describe 'as_json' do
     it 'should produce a json representation of the contacts' do
-      expect(cc.as_json).to eq expected_json(cc)
+      expect(cc.as_json).to eq expected_claimant_collected_json(cc)
     end
   end
 
@@ -131,13 +131,13 @@ describe ClaimantCollection do
 end
 
 
-def expected_json(cc)
+def expected_claimant_collected_json(cc)
   {'claimant_1' => cc[1].as_json, 'claimant_2' => cc[2].as_json, 'claimant_3' => cc[3].as_json }.as_json
 end
 
 
 
-def claim_params
+def test_claim_params
   HashWithIndifferentAccess.new(
     { "num_claimants" => 3,
       "claimant_type" => 'individual',
