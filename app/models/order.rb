dd40b3122af41f1c6b4020ef95cc9c7859d475aa
Rob McKinnon <@digital.justice.gov.uk>
2014-09-04 12:11:17 +0100

    validation error message for defendant paying the cost of the claim question

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,12 +2,12 @@ class Order < BaseClass
 
   attr_accessor :possession
   validates :possession, presence: true
-  validates :possession, inclusion: { in: ['Yes'], message: 'Please tick to confirm that you want to repossess the property' }
+  validates :possession, inclusion: { in: ['Yes'] }
 
   before_validation :set_possession_to_yes
 
   attr_accessor :cost
-  validates :cost, presence: true
+  validates :cost, presence: { message: 'You must choose whether you wish to ask the defendant to pay the cost of this claim' }
   validates :cost, inclusion: { in: ['Yes', 'No'] }
 
   private
