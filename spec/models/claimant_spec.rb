4ea0d02e3137af90fc3c995914545b48ecdf0322
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2014-09-11 15:45:29 +0100

    removed repeated validation specs

--- spec/models/claimant_spec.rb
+++ spec/models/claimant_spec.rb
@@ -1,6 +1,6 @@
 describe Claimant, :type => :model do
 
-  def params
+  def individual_params
     HashWithIndifferentAccess.new(
       title: 'Mr',
       full_name: "John Doe",
@@ -11,16 +11,19 @@ describe Claimant, :type => :model do
     )
   end
 
-  let(:claimant_params) { params }
+  def organisation_params
+    individual_params.merge(
+      title: nil,
+      full_name: nil,
+      organization_name: 'Anytown Council Housing Departement',
+      claimant_type: 'organization',
+      claimant_num: 2
+    )
+  end
 
   let(:claimant) { Claimant.new(claimant_params) }
 
-  subject { claimant }
-
-  it { is_expected.to be_valid }
-
   context 'equality comparison' do
-
     it 'should be true if two new objects are compared with one another' do
       c1 = Claimant.new
       c2 = Claimant.new
@@ -28,14 +31,14 @@ describe Claimant, :type => :model do
     end
 
     it 'should be true for two claimants with the same values' do
-      c1 = Claimant.new( params )
-      c2 = Claimant.new( params )
+      c1 = Claimant.new( individual_params )
+      c2 = Claimant.new( individual_params )
       expect(c1).to eq c2
     end
 
     it 'should be false if any of the instance variables are different' do
-      c1 = Claimant.new( params )
-      c2 = Claimant.new( params.merge("title" => "XXX") )
+      c1 = Claimant.new( individual_params )
+      c2 = Claimant.new( individual_params.merge("title" => "XXX") )
       expect(c1).not_to eq c2
     end
   end
@@ -47,38 +50,9 @@ describe Claimant, :type => :model do
     end
   end
 
-  context 'validate_presence set to true' do
-
-    it 'should set the validate_presence attribute to true if missing' do
-      expect(claimant.validate_presence).to be true
-    end
-
-    it 'should be valid if all attributes are set' do
-      expect(claimant).to be_valid
-    end
-
-    it 'should not be valid if any of the attributes is missing' do
-      claimant.full_name = nil
-      claimant.street = nil
-      expect(claimant).to_not be_valid
-      expect(claimant.errors[:full_name]).to eq [ "Enter claimant 1's full name" ]
-      expect(claimant.errors[:street]).to eq ["Enter claimant 1's full address"]
-    end
-
-    it 'should not be valid if any of the attributes are blank' do
-      claimant.title = ''
-      claimant.full_name = ''
-      claimant.street = ''
-      claimant.postcode = ''
-      expect(claimant).to_not be_valid
-      expect(claimant.errors[:title]).to eq ["Enter claimant 1's title"]
-      expect(claimant.errors[:full_name]).to eq ["Enter claimant 1's full name"]
-      expect(claimant.errors[:street]).to eq ["Enter claimant 1's full address"]
-      expect(claimant.errors[:postcode]).to eq ["Enter claimant 1's postcode"]
-    end
-  end
-
   context 'validate_absence set to true' do
+    let(:claimant_params) { individual_params }
+    subject { claimant }
 
     context 'when all attributes are empty' do
       let(:claimant_params) { { validate_absence: true, claimant_num: 1 } }
@@ -86,22 +60,14 @@ describe Claimant, :type => :model do
     end
 
     context 'when attributes are present' do
-      let(:claimant_params) { params.merge(validate_absence: true) }
+      let(:claimant_params) { individual_params.merge(validate_absence: true) }
       it { is_expected.to_not be_valid }
     end
   end
 
   context 'claimant_type organization' do
-    let(:claimant_params) do
-      params.merge(
-        title: nil,
-        full_name: nil,
-        organization_name: 'Anytown Council Housing Departement',
-        claimant_type: 'organization',
-        claimant_num: 2
-      )
-    end
-
+    let(:claimant_params) { organisation_params }
+    subject { claimant }
     it { is_expected.to be_valid }
 
     describe 'mandatory fields for organizations are present' do
@@ -125,29 +91,35 @@ describe Claimant, :type => :model do
     end
   end
 
-  context 'when claimant number 2' do
-    let(:claimant_params) { params.merge(claimant_num: 2) }
+  context 'claimant_type individual' do
+    let(:claimant_params) { individual_params.merge(claimant_num: 2) }
 
-    context 'mandatory fields for individuals are present' do
-      it { is_expected.to be_valid }
+    it 'should set the validate_presence attribute to true if missing' do
+      expect(claimant.validate_presence).to be true
+    end
 
-      it 'should not be valid when full name is missing' do
-        claimant.full_name = nil
-        expect(claimant).not_to be_valid
-        expect(claimant.errors[:full_name]).to eq ["Enter claimant 2's full name"]
-      end
+    it 'should be valid if all attributes are set' do
+      expect(claimant).to be_valid
+    end
 
-      it 'should not be valid when street is missing' do
-        claimant.street = nil
-        expect(claimant).not_to be_valid
-        expect(claimant.errors[:street]).to eq ["Enter claimant 2's full address"]
-      end
+    it 'should not be valid if any of the attributes is missing' do
+      claimant.full_name = nil
+      claimant.street = nil
+      expect(claimant).to_not be_valid
+      expect(claimant.errors[:full_name]).to eq [ "Enter claimant 2's full name" ]
+      expect(claimant.errors[:street]).to eq ["Enter claimant 2's full address"]
+    end
 
-      it 'should not be valid when postcode is missing' do
-        claimant.postcode = nil
-        expect(claimant).not_to be_valid
-        expect(claimant.errors[:postcode]).to eq ["Enter claimant 2's postcode"]
-      end
+    it 'should not be valid if any of the attributes are blank' do
+      claimant.title = ''
+      claimant.full_name = ''
+      claimant.street = ''
+      claimant.postcode = ''
+      expect(claimant).to_not be_valid
+      expect(claimant.errors[:title]).to eq ["Enter claimant 2's title"]
+      expect(claimant.errors[:full_name]).to eq ["Enter claimant 2's full name"]
+      expect(claimant.errors[:street]).to eq ["Enter claimant 2's full address"]
+      expect(claimant.errors[:postcode]).to eq ["Enter claimant 2's postcode"]
     end
 
     describe 'address validation' do
