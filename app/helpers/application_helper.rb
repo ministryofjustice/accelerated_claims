c6577b238b5dc50a4f1bbf7d3725c27800e28767
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-09-11 16:47:22 +0100

    move claimant_header_helper into application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,4 +45,12 @@ module ApplicationHelper
     end
   end
 
+  def claimant_header claimant_id
+    if claimant_id == 1
+      "<h3>Claimant #{claimant_id}</h3>".html_safe
+    else
+      "<h3>Claimant #{claimant_id} <span class='hint hide js-claimanttype'>(optional)</span></h3>".html_safe
+    end
+  end
+
 end
