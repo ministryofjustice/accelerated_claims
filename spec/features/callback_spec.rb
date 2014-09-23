908e9086dffbba196287f248c29c1e7667681ed6
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2014-09-23 10:03:57 +0100

    Make sure we wait for redirect before we check page content in callback spec

--- spec/features/callback_spec.rb
+++ spec/features/callback_spec.rb
@@ -11,16 +11,21 @@ end
 feature 'Callback request' do
 
   context 'with phone number' do
+    let(:expected_url) { remote_test? ? '/accelerated-possession-eviction' : '/' }
+
     scenario 'request feedback successfully' do
       visit '/ask-for-technical-help'
 
       fill_in_the_form_correctly
 
+      expect(current_path).to eq expected_url
       expect(page).to have_content('Thank you we will call you back during the next working day between 9am and 5pm.')
     end
   end
 
   context 'without phone number' do
+    let(:expected_url) { remote_test? ? '/accelerated-possession-eviction/user_callback_request' : '/' }
+
     scenario 'request feedback successfully' do
       visit '/ask-for-technical-help'
 
@@ -30,6 +35,7 @@ feature 'Callback request' do
 
       click_button 'Send'
 
+      expect(current_path).to eq expected_url
       expect(page).to have_content('Please enter a valid phone number.')
     end
   end
@@ -44,8 +50,9 @@ feature 'Callback request' do
 
       fill_in_the_form_correctly
 
-      expect(page).to have_content('Thank you we will call you back during the next working day between 9am and 5pm.')
       expect(current_path).to eq expected_url
+      expect(page).to have_content('Thank you we will call you back during the next working day between 9am and 5pm.')
+
     end
 
     scenario 'go back to form page' do
@@ -55,9 +62,9 @@ feature 'Callback request' do
 
       fill_in_the_form_correctly
 
+      expect(current_path).to eq expected_url
       expect(page).to have_content('Thank you we will call you back during the next working day between 9am and 5pm.')
 
-      expect(current_path).to eq expected_url
     end
 
     scenario 'go back to technical help page' do
@@ -67,9 +74,9 @@ feature 'Callback request' do
 
       fill_in_the_form_correctly
 
+      expect(current_path).to eq expected_url
       expect(page).to have_content('Thank you we will call you back during the next working day between 9am and 5pm.')
 
-      expect(current_path).to eq expected_url
     end
   end
 end
