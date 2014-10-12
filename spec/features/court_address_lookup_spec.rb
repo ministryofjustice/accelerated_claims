849f589cdd37a0305a7f4542d60b19c277dcc240
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-10-12 08:38:11 +0100

    Specs for Court Address fetching and population

--- spec/features/court_address_lookup_spec.rb
+++ spec/features/court_address_lookup_spec.rb
@@ -25,27 +25,39 @@ feature 'Court address lookup' do
   end
 
   context 'when property address is populated' do
-    scenario 'it should find and populate court name', js: true do
-      postcode = 'SG8 0LT'
-      json = [
-              {
-                'name' => 'Cambridge County Court and Family Court',
-                'address' => {
-                  'town' => 'Cambridge',
-                  'address_lines' => ['Cambridge County Court and Family Court Hearing Centre',
-                                      '197 East Road'],
-                  'type' => 'Postal',
-                  'postcode' => 'CB1 1BA',
-                  'county' => 'Cambridgeshire'
-                }
-              }
-             ].to_json
+    let(:postcode) { 'SG8 0LT' }
+    let(:json) {
+      [
+       {
+         'name' => 'Cambridge County Court and Family Court',
+         'address' => {
+           'town' => 'Cambridge',
+           'address_lines' => ['Cambridge County Court and Family Court Hearing Centre',
+                               '197 East Road'],
+           'type' => 'Postal',
+           'postcode' => 'CB1 1BA',
+           'county' => 'Cambridgeshire'
+         }
+       }
+      ].to_json
+    }
 
-      court_finder_stub(postcode, body: json)
+    before { court_finder_stub(postcode, body: json) }
+
+    scenario 'it should find and populate court name', js: true do
       visit '/'
       fill_in 'claim_property_postcode', with: postcode
-
       expect(page).to have_text 'Cambridge County Court and Family Court'
+    end
+
+    scenario 'it should find and populate court address details in the hidden form', js: true do
+      pending "can't read the #claim_court_address hidden field's value"
+      # TODO: fix this
+      visit '/'
+      fill_in 'claim_property_postcode', with: postcode
+      json_address = JSON.parse(json)[0]['address']['address_lines'].join(',')
+      address = find("#claim_court_address", visible: false).value
+      expect(address).to eq json_address
     end
   end
 end
