ea5c7502aecc7c7b893b190e91edf49dfc2f1f3b
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2014-10-17 15:03:11 +0100

    fix journey features to show validation errors on failure of claim submission

--- spec/support/fill_in_helper.rb
+++ spec/support/fill_in_helper.rb
@@ -11,3 +11,13 @@ def fill_text_area id, value
   fill_in_value 'textarea', id, value
 end
 
+def validation_error_text page
+  error_messages = page.all('.error-summary li')
+  if error_messages.size > 0
+    errors = ["Expected successful claim submission, instead these validation errors were returned:"]
+    error_messages.each { |li| errors << "#{li.text}: #{li.find('a')['href']}" }
+    errors.join("\n\t")
+  else
+    nil
+  end
+end
