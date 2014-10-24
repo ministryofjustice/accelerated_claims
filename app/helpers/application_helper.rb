e8dbb62b1f90d03ee1137d80a47fe179231f6fd0
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2014-10-24 12:02:49 +0100

    added summary_label() summary_value() view helpers and more changes to confirmation summary content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,29 @@ module ApplicationHelper
     end
   end
 
+  def localization_key section, label, suffix
+    "claim.#{section}.#{label.sub(/_\d+/,'')}.#{suffix}"
+  end
+
+  def summary_value section, label, value
+    key = localization_key(section, label, value.to_s.downcase)
+    localized_value = I18n.t(key)
+
+    unless localized_value.is_a?(Hash) || localized_value[/translation missing/]
+      value = localized_value.split('<span').first.html_safe
+    end
+    value
+  end
+
+  def summary_label section, label
+    key = localization_key(section, label, 'label')
+    if I18n.t(key)[/translation missing/]
+      label.humanize
+    else
+      I18n.t(key)
+    end
+  end
+
   def link_reference_for_error field
     case field
     # links applicable statement errors to top of applicable statement section
