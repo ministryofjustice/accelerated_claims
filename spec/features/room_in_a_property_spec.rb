f7de9c6b4b7aa0a58e06873be6a96230b1cd2a7c
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-10-30 11:32:08 +0000

    un pend tests

--- spec/features/room_in_a_property_spec.rb
+++ spec/features/room_in_a_property_spec.rb
@@ -11,35 +11,25 @@ feature 'Filling in property section' do
   unless remote_test?
     scenario "see room number hint", js: false do
       visit '/'
-      expect(page).to have_content('If "Room(s) in a property" was selected above, include the room number')
-    end
-
-    scenario "see room number hint", js: false do
-      visit '/'
       expect(page).to have_content('If "Room or rooms in a property" was selected above, include the room number')
     end
 
     scenario "selecting room in a property, then selecting self-contained house", js: true do
       visit '/'
+      expect(page).to have_css('div#javascript_done')
       expect(page).not_to have_content('Include the room number')
-
       choose('claim_property_house_no')
+
+      fill_in 'claim_property_postcode_edit_field', with: 'rg27pu'
+      click_link 'Find address'
+      select "160 Northumberland Avenue, READING", from: "sel-address"
+      click_link "claim_property_selectaddress"
       expect(page).to have_content('Include the room number')
 
       choose('claim_property_house_yes')
       expect(page).not_to have_content('Include the room number')
 
       choose('claim_property_house_no')
-      expect(page).to have_content('Include the room number')
-    end
-
-    scenario "selecting room in a property and submitting", js: true do
-      visit '/'
-      choose('claim_property_house_no')
-      expect(page).to have_content('Include the room number')
-
-      click_button 'Continue'
-      expect(page).to have_content('Enter the full address')
       expect(page).to have_content('Include the room number')
     end
   end
