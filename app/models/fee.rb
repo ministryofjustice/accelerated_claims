827fe5e234bd40ac6c7bd0571e3c825006d8ae99
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-10-30 16:49:41 +0000

    added fee account field to fee with specs

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,10 +1,20 @@
 class Fee < BaseClass
 
   attr_accessor :court_fee
+  attr_accessor :fee_account
+
   validates :court_fee, presence: { message: 'must be entered' }, length: { maximum: 8 }
+  validates :fee_account, length: { is: 10 }, allow_blank: true
+
+  before_validation :zero_pad_fee_account
 
   def court_fee
     "280.00"
   end
 
+  private
+
+  def zero_pad_fee_account
+    self.fee_account = self.fee_account.to_s.rjust(10, '0') unless self.fee_account.blank?
+  end
 end
--- spec/models/fee_spec.rb
+++ spec/models/fee_spec.rb
@@ -1,24 +1,43 @@
 describe Fee, :type => :model do
-  let(:fee) { Fee.new(court_fee: court_fee) }
+  let(:fee) { Fee.new(court_fee: court_fee, fee_account: fee_account) }
 
   subject { fee }
 
   context "with blank court fee" do
     let(:court_fee) { "" }
+    let(:fee_account) { '' }
 
     it { is_expected.to be_valid }
     it { expect(fee.court_fee).to eq "280.00"  }
 
-    it { expect(fee.as_json).to eq("court_fee" => "280.00") }
+    it { expect(fee.as_json).to eq("court_fee" => "280.00", "fee_account"=>"") }
   end
 
   context "with a random court fee" do
     let(:court_fee) { 250 }
+    let(:fee_account) { '' }
 
     it { is_expected.to be_valid }
     it { expect(fee.court_fee).to eq "280.00"  }
 
-    it { expect(fee.as_json).to eq("court_fee" => "280.00") }
+    it { expect(fee.as_json).to eq("court_fee" => "280.00", "fee_account"=>"") }
+  end
+
+  context 'with a fee account number' do
+    let(:fee_account) { 9876543210 }
+    let(:court_fee) { 250 }
+
+    it { is_expected.to be_valid }
+  end
+
+  context 'with a short account number' do
+    let(:fee_account) { '1234' }
+    let(:court_fee) { 250 }
+
+    it 'is expected to be zero padded' do
+      expect(fee).to be_valid
+      expect(fee.fee_account).to eq '0000001234'
+    end
   end
 
 end
