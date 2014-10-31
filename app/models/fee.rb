db6cf7123314bfac486b11840d07e6b7f415f6c3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-10-31 12:52:47 +0000

    renamed fee_account to account to prevent fee_fee_account

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,17 +1,17 @@
 class Fee < BaseClass
 
   attr_accessor :court_fee
-  attr_accessor :fee_account
+  attr_accessor :account
 
   validates :court_fee, presence: { message: 'must be entered' }, length: { maximum: 8 }
-  validates :fee_account, length: { is: 10 }, allow_blank: true
+  validates :account, length: { is: 10 }, allow_blank: true
 
 
   def court_fee
     "280.00"
   end
 
-  def fee_account
-    @fee_account.blank? ? '' : @fee_account.to_s.rjust(10, '0')
+  def account
+    @account.blank? ? '' : @account.to_s.rjust(10, '0')
   end
 end
--- spec/models/fee_spec.rb
+++ spec/models/fee_spec.rb
@@ -1,41 +1,40 @@
 describe Fee, :type => :model do
-  let(:fee) { Fee.new(court_fee: court_fee, fee_account: fee_account) }
+  let(:fee) { Fee.new(court_fee: court_fee, account: account) }
 
   subject { fee }
 
   context "with blank court fee" do
     let(:court_fee) { "" }
-    let(:fee_account) { '' }
+    let(:account) { '' }
 
     it { is_expected.to be_valid }
     it { expect(fee.court_fee).to eq "280.00"  }
 
-    it { expect(fee.as_json).to eq("court_fee" => "280.00", "fee_account"=>"") }
+    it { expect(fee.as_json).to eq("court_fee" => "280.00", "account"=>"") }
   end
 
   context "with a random court fee" do
     let(:court_fee) { 250 }
-    let(:fee_account) { '' }
+    let(:account) { '' }
 
     it { is_expected.to be_valid }
     it { expect(fee.court_fee).to eq "280.00"  }
 
-    it { expect(fee.as_json).to eq("court_fee" => "280.00", "fee_account"=>"") }
+    it { expect(fee.as_json).to eq("court_fee" => "280.00", "account"=>"") }
   end
 
   context 'with a fee account number' do
-    let(:fee_account) { 9876543210 }
-    let(:court_fee) { 250 }
+    let(:court_fee) { "" }
+    let(:account) { 9876543210 }
 
     it { is_expected.to be_valid }
-    it { expect(fee.as_json).to eq("court_fee" => "280.00", "fee_account"=>"9876543210") }
+    it { expect(fee.as_json).to eq("court_fee" => "280.00", "account"=>"9876543210") }
   end
 
   context 'with a short account number' do
-    let(:fee_account) { '1234' }
-    let(:court_fee) { 250 }
+    let(:court_fee) { "" }
+    let(:account) { '1234' }
     it { is_expected.to be_valid }
-    it { expect(fee.as_json).to eq("court_fee" => "280.00", "fee_account"=>"0000001234") }
+    it { expect(fee.as_json).to eq("court_fee" => "280.00", "account"=>"0000001234") }
   end
-
 end
