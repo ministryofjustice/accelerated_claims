a04164d59072b272e0ba76b081276e66dbb346cb
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-11-11 16:20:00 +0000

    Added court address to the summary page
    Court name, address & postcode is displayed in the summary page.
    The fixtures updated also.

--- spec/fixtures/scenario_11_summary_results.rb
+++ spec/fixtures/scenario_11_summary_results.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 {
   claim_property_house: [
     "Type of property",
@@ -170,5 +171,17 @@
   claim_possession_hearing: [
     "Claimant to be at court if there's a hearing",
     "Yes"
+  ],
+  claim_court_name: [
+    "Name",
+    "court_name"
+  ],
+  claim_court_address: [
+    "Address",
+    "court_street"
+  ],
+  claim_court_postcode: [
+    "Postcode",
+    "postcode"
   ]
 }
