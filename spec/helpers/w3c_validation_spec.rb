0a6da53dd61a27a8183614cfe299a28864b4e120
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-11-13 12:18:33 +0000

    created test dependant on ENV[w3c_validate]

--- spec/helpers/w3c_validation_spec.rb
+++ spec/helpers/w3c_validation_spec.rb
@@ -1,37 +1,16 @@
-require 'w3c_validators'
-
-include W3CValidators
-
 describe ClaimController, :type => :controller do
   render_views
-  describe '#confirmation' do
-    context 'with valid claim data' do
-      it 'should render the confirmation page' do
-        @validator = MarkupValidator.new
+  if ENV['w3c_validate']
 
-        # override the DOCTYPE
-        @validator.set_doctype!(:html32)
-
-        # turn on debugging messages
-        @validator.set_debug!(true)
-
+    describe '#confirmation' do
+      context 'with valid claim data' do
+        it 'should render the confirmation page' do
         @controller.session['claim'] = claim_post_data['claim']
         get :confirmation
         expect(response).to render_template("confirmation")
-        results = @validator.validate_file(response.body)
 
-        if results.errors.length > 0
-          results.errors.each do |err|
-            puts err.to_s
-          end
-        else
-          puts 'Valid!'
-        end
-
-        puts 'Debugging messages'
-
-        results.debug_messages.each do |key, value|
-          puts "#{key}: #{value}"
+          page_valid = validate_view(response,{w3c_debug: ENV['w3c_debug'] || false})
+          expect(page_valid.errors.length).to eql(0)
         end
       end
     end
