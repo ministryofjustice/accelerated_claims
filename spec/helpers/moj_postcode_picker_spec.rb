fa3c41217a349e82f2ec366f84027509e30e9333
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-11-19 11:01:46 +0000

    fix spacing issues on expected output

--- spec/helpers/moj_postcode_picker_spec.rb
+++ spec/helpers/moj_postcode_picker_spec.rb
@@ -56,9 +56,17 @@ describe 'MojPostcodePicker' do
       expect(mpp.instance_variable_get(:@address_attr)).to eq 'street'
     end
 
+    it 'generates non-default button label' do
+      allow(object).to receive(:postcode).and_return('RG2 7PU')
+      allow(object).to receive(:street).and_return("50 Tregunter Road\r\nLondon")
+      mpp = MojPostcodePicker.new(form, prefix: "claim_claimant_1", name: "claim[claimant_1]", address_attr: 'street', button_label: "Chercher l'adresse")
+
+      expect(mpp.instance_variable_get(:@button_label)).to eq "Chercher l'adresse"
+    end
+
   end
 
-  describe 'emit' do
+  describe "emit" do
     it 'substitutes its own variables' do
       allow(object).to receive(:postcode).and_return('RG2 7PU')
       allow(object).to receive(:street).and_return("50 Tregunter Road\r\nLondon")
