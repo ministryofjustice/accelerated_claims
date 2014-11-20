9d722469d21edfe855067aca708c6e7ef36ec8f6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-11-20 15:40:36 +0000

    updated validation to test to remove render test

--- spec/helpers/w3c_validation_spec.rb
+++ spec/helpers/w3c_validation_spec.rb
@@ -1,14 +1,11 @@
 describe ClaimController, :type => :controller do
   render_views
   if ENV['w3c_validate']
-
     describe '#confirmation' do
       context 'with valid claim data' do
-        it 'should render the confirmation page' do
+        it 'should pass validation' do
           @controller.session['claim'] = claim_post_data['claim']
           get :confirmation
-          expect(response).to render_template("confirmation")
-
           page_valid = validate_view(response,{w3c_debug: ENV['w3c_debug'] || false})
           expect(page_valid.errors.length).to eql(0)
         end
