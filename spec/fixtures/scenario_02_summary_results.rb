2185839eb54f43fdaea31e4af0ae28f5a19641a4
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2014-11-21 15:26:19 +0000

    applying govuk elements styles to confirmation summary; added new fields to summary results fixture files

--- spec/fixtures/scenario_02_summary_results.rb
+++ spec/fixtures/scenario_02_summary_results.rb
@@ -12,6 +12,14 @@
     "Postcode",
     "FX1X 9ZZ"
   ],
+  claim_claimant_type: [
+    "Type of claimant",
+    "A private landlord (individual)"
+  ],
+  claim_claimant_num_claimants: [
+    "Number of claimants",
+    "1"
+  ],
   claim_claimant_1_title: [
     "Title",
     "Mr"
@@ -55,6 +63,10 @@
   claim_claimant_contact_postcode: [
     "Postcode",
     "FX88 1AA"
+  ],
+  claim_defendant_num_defendants: [
+    "Number of defendants",
+    "1"
   ],
   claim_defendant_1_title: [
     "Title",
