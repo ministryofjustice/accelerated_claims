fdaba10e37b98dcc14a233c683f51e2df38a43ba
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-11-26 16:35:42 +0000

    tests passing

--- lib/tasks/w3c.rake
+++ lib/tasks/w3c.rake
@@ -1,17 +1,6 @@
 namespace :w3c do
-  desc 'Checks each view against remote w3c validator'
-  task validate: :environment do
-    ENV['w3c_validate'] = 'true'
-    task :validate_task => []
-    Rake::Task[:validate_task].clear
-    RSpec::Core::RakeTask.new(:validate_task) do |t|
-      t.pattern = 'spec/features/w3c_validation_spec.rb'
-    end
-    Rake::Task[:validate_task].invoke
-  end
-
   desc 'Call one task that loads the data and loops'
-  task validate_data_file: :environment do
+  task validate: :environment do
     ENV['w3c_validate']='true'
     task :validate_task => []
     Rake::Task[:validate_task].clear
@@ -20,6 +9,5 @@ namespace :w3c do
     end
     Rake::Task[:validate_task].invoke
   end
-
 end
 
--- spec/support/w3c_validator.rb
+++ spec/support/w3c_validator.rb
@@ -2,7 +2,7 @@ require 'w3c_validators'
 
 include W3CValidators
 
-def validate_view(response, options)
+def validate_view(response, options={})
   WebMock.disable_net_connect!(:allow => [ /validator.w3.org/ ])
   @validator = MarkupValidator.new
 
@@ -12,36 +12,30 @@ def validate_view(response, options)
   outcome.errors={}
 
   tries = options[:attempts] || 3
-  attempt = 1
+  attempt = 0
 
   begin
+    attempt += 1
     begin
       results = @validator.validate_text(response.body)
-
       if results.errors.length > 0
         outcome.result='fail'
         outcome.errors = results.errors
-        outcome.message = '\nValidation failed'
-        outcome.message += '\nErrors'
-        outcome.message += '\n------------------'
+        outcome.message = "\nValidation failed"
+        outcome.message += "\nErrors"
+        outcome.message += "\n------------------"
         results.errors.each do |err|
           outcome.message += "\n#{err.to_s}"
-        end
-        outcome.message += '\nDebugging messages'
-        outcome.message += '\n------------------'
-        results.debug_messages.each do |key, value|
-          outcome.message += "\n   #{key}: #{value}"
         end
       else
         outcome.result = 'pass'
       end
     rescue => e
       outcome.result='error'
-      outcome.message =  '\nError while validating'
-      outcome.message += '\n----------------------'
+      outcome.message =  "\nError while validating"
+      outcome.message += "\n----------------------"
       outcome.message += "\n#{e.message}"
     end
-    attempt += 1
   end while outcome.result == 'error' &&  attempt < tries
   if attempt==tries
     outcome.result='timeout'
