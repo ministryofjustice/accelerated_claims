fb1a810061ec33e79306f1ce8f244990ecc0dea4
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-11-27 12:25:44 +0000

    remove commented-out lines

--- app/models/property.rb
+++ app/models/property.rb
@@ -10,7 +10,6 @@ class Property < BaseClass
   validates :house, presence: { message: 'Please select what kind of property it is' }, inclusion: { in: ['Yes', 'No'] }
   validate :address_validation
 
-
   def initialize(params)
     @params = params
     @livepc = params['livepc'] || false
@@ -22,7 +21,6 @@ class Property < BaseClass
   def address_validation
     @address.valid?               # This takes care of transferring error messages from Address object to Property object
   end
-
 
   def as_json
     postcode1, postcode2 = split_postcode
@@ -41,35 +39,4 @@ class Property < BaseClass
   def possessive_subject_description
     subject_description
   end
-
-  private
-
-  # def postcode_is_in_england_or_wales
-  #   if postcode.blank? || UKPostcode.new(postcode).valid? == false
-  #     errors['postcode'] << "Please enter a valid postcode for a property in England and Wales"
-  #     return false
-  #   end
-
-  #   if postcode.present?
-  #     plp = PostcodeLookupProxy.new(postcode, ['England', 'Wales'], @livepc)
-  #     plp.lookup
-  #     @postcode = plp.norm
-
-  #     case plp.result_set['code']
-  #     when 2000
-  #       true
-  #     when 4040
-  #       true
-  #     when 4041
-  #       errors['postcode'] << "Postcode is in #{plp.result_set['message']}. You can only use this service to regain possession of properties in England and Wales."
-  #       false
-  #     when 4220
-  #       true
-  #     when 5030
-  #       true
-  #     else
-  #       raise "Unexpected return from postcode lookup: #{plp.result_set.inspect}"
-  #     end
-  #   end
-  # end
 end
