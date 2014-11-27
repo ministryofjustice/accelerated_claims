15576b284f5b550d735ffa6e87cff61c22725940
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-11-27 11:11:45 +0000

    removed un-needed code

--- app/models/w3c_validation_result.rb
+++ app/models/w3c_validation_result.rb
@@ -5,33 +5,4 @@ class W3cValidationResult
   attr_accessor :errors
   attr_accessor :message
 
-  attr_accessor :timeout
-  attr_accessor :fails
-  attr_accessor :passes
-
-  def initialize
-    self.fails=0
-    self.timeout=0
-    self.passes=0
-  end
-
-  def increment_fail
-    self.fails = self.fails+1
-  end
-  def increment_passes
-    self.passes = self.passes+1
-  end
-  def increment_errors
-    self.errors = self.errors+1
-  end
-
-  def outcome
-    if self.fails > 0
-      'Validation failed'
-    elsif self.timeout>0
-      'Validation passed with timeouts'
-    else
-      'Validation passed'
-    end
-  end
 end
