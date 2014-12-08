e3a33ea8429f8b50844a31815ab77232248c2f77
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-12-08 14:47:12 +0000

    Log any errors from Courtfinder client
    The reason for putting this in place is to log all the errors that the
    Courtfinder API might return. The client gem's implementation tries to
    handle most common situations.

--- spec/controllers/courtfinder_controller_spec.rb
+++ spec/controllers/courtfinder_controller_spec.rb
@@ -32,5 +32,35 @@ describe CourtfinderController, type: :controller do
         expect(response.body).to eq "No court found for #{postcode} postcode"
       end
     end
+
+    context 'when error is returned' do
+      let(:postcode) { 'foo bar' }
+      let(:json_error) { '{ "error": "Timeout" }' }
+
+      before do
+        ENV['ENV_NAME'] = 'production'
+        WebMock.disable_net_connect!(:allow => ["127.0.0.1", /codeclimate.com/])
+        instance_double('Courtfinder::Client::HousingPossession', get: postcode)
+      end
+
+      after { ENV.delete('ENV_NAME') }
+
+      def custom_stub(body)
+        url = "https://courttribunalfinder.service.gov.uk/search/results.json?aol=Housing%20possession&postcode=#{postcode}"
+        stub_request(:get, url).to_return(status: 400, body: body, headers: {})
+      end
+
+      it 'should log the error' do
+        custom_stub(json_error)
+        expect(LogStuff).to receive(:error)
+        get :address, postcode: postcode
+      end
+
+      it 'should return expected result' do
+        custom_stub(json_error)
+        get :address, postcode: postcode
+        expect(response.body).to eq "No court found for #{postcode} postcode"
+      end
+    end
   end
 end
