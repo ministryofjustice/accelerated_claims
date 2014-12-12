ef94bc584fedb0095451249b295e9ba1e2b387fc
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-12-12 16:14:09 +0000

    Indentation fix

--- spec/support/court_finder.rb
+++ spec/support/court_finder.rb
@@ -6,15 +6,15 @@ end
 
 def court_address
   [{
-     'name' => 'Cambridge County Court and Family Court',
-     'address' => {
-       'town' => 'Cambridge',
-       'address_lines' => ['Cambridge County Court and Family
-                            Court Hearing Centre'.sub('  ', ''),
-                           '197 East Road'],
-       'type' => 'Postal',
-       'postcode' => 'CB1 1BA',
-       'county' => 'Cambridgeshire'
-     }
+      'name' => 'Cambridge County Court and Family Court',
+      'address' => {
+        'town' => 'Cambridge',
+        'address_lines' => ['Cambridge County Court and Family
+                             Court Hearing Centre'.sub('  ', ''),
+                            '197 East Road'],
+        'type' => 'Postal',
+        'postcode' => 'CB1 1BA',
+        'county' => 'Cambridgeshire'
+      }
    }]
 end
