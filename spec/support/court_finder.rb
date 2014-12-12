618d1ebd71ef2cea2386a4000dc0d5eff05c8375
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-12-12 16:02:31 +0000

    Line length fix

--- spec/support/court_finder.rb
+++ spec/support/court_finder.rb
@@ -9,7 +9,8 @@ def court_address
      'name' => 'Cambridge County Court and Family Court',
      'address' => {
        'town' => 'Cambridge',
-       'address_lines' => ['Cambridge County Court and Family Court Hearing Centre',
+       'address_lines' => ['Cambridge County Court and Family
+                            Court Hearing Centre'.sub('  ', ''),
                            '197 East Road'],
        'type' => 'Postal',
        'postcode' => 'CB1 1BA',
