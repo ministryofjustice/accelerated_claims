26383447a81324344545521d1a70b5d48c3bd9a0
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-12-18 14:57:56 +0000

    Switched the hash syntax to Ruby 1.9 style

--- spec/routing/maintenance_spec.rb
+++ spec/routing/maintenance_spec.rb
@@ -1,4 +1,4 @@
-describe 'maintenance route', :type => :routing do
+describe 'maintenance route', type: :routing do
   it 'routes /maintenance to the static#maintenance' do
     expect(get(maintenance_path)).to route_to('static#maintenance')
   end
