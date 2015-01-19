b7402b9273aed3204e7d63a953195e2c23f0b60f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-01-19 13:46:35 +0000

    added helper for external links

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -12,5 +12,10 @@ describe 'ApplicationHelper', type: :helper  do
       expect(header).to eq "<h2 class='section-header' id='claimant-section'>Claimants</h2>\n"
     end
   end
-
+  describe 'external_link' do
+    it 'should have set alt text' do
+      link = helper.external_link 'http://www.text.com', 'test'
+      expect(link).to eq("<a href='http://www.text.com' class='external' rel='external' target='_blank' alt='External link, opens in new window'>test</a>")
+    end
+  end
 end
