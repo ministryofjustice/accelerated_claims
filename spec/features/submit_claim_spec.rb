891e878c39f4877e255f6674e0ce18919d826f7d
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-02-17 11:55:33 +0000

    re-enable all journeys in submit_claim_spec

--- spec/features/submit_claim_spec.rb
+++ spec/features/submit_claim_spec.rb
@@ -80,8 +80,7 @@ feature "submit claim" do
 
   JOURNEY = ENV['JOURNEY'] || ''
 
-  # Dir.glob("spec/fixtures/scenario_#{JOURNEY}*_data.rb") do |data_file|
-  Dir.glob("spec/fixtures/scenario_01*_data.rb") do |data_file|
+  Dir.glob("spec/fixtures/scenario_#{JOURNEY}*_data.rb") do |data_file|
     data = load_fixture_data(data_file)
     title = data['title']
     description = data['description']
