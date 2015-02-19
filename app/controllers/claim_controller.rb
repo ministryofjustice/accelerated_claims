c4636c6686bb101e93b8a78ec088919f75738c58
Stephen Richards <stephen@mimi.local>
2015-02-19 17:02:56 +0000

    remove test debugging

--- app/controllers/claim_controller.rb
+++ app/controllers/claim_controller.rb
@@ -67,7 +67,6 @@ class ClaimController < ApplicationController
   end
 
   def submission
-    puts ">>>>>>>>>>>>>> claims_controller # submission <<<<<<<<<<<<<<<<<<"
     params['claim']['use_live_postcode_lookup'] = @use_live_postcode_lookup
     session[:claim] = params['claim']
 
